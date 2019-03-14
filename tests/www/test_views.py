f68ca0a33218976aee968292e335eaea531c06e2
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-03-14 14:50:41 +0000

    Backport [AIRFLOW-4083] Add tests for link generation utils (#4912)
    We were making use of the "bleach" module or jinja.escape function to
    clean parameters when it wasn't needed - we could simply call .format on
    the Markup object and it will handle escaping for us. (format the
    object, not format the string passed to the constructor)
    
    This removes the (direct?) dependency on bleach - one less thing to
    depend on is a good thing too.
    
    Change-Id: I85b956d11a10ac1165610f2b1d759ed59a013bff
    GitOrigin-RevId: 1c2b72e4c36b5852a763fe4cd269b50d2bc938e2

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -31,6 +31,7 @@ import json
 from urllib.parse import quote_plus
 from werkzeug.test import Client
 
+import airflow
 from airflow import models, configuration, settings
 from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG
 from airflow.models import DAG, DagRun, TaskInstance
@@ -788,6 +789,65 @@ class TestTaskInstanceView(unittest.TestCase):
         # in to flask-admin) but simply that our UTC conversion was run - i.e. it
         # doesn't blow up!
         self.assertEqual(resp.status_code, 200)
+
+
+class HelpersTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = application.create_app(testing=True)
+
+        airflow.load_login()
+        # Delay this import until here
+        import airflow.www.views as views
+        cls.views = views
+
+    def test_state_token(self):
+        # It's shouldn't possible to set these odd values anymore, but lets
+        # ensure they are escaped!
+        html = str(self.views.state_token('<script>alert(1)</script>'))
+
+        self.assertIn(
+            '&lt;script&gt;alert(1)&lt;/script&gt;',
+            html,
+        )
+        self.assertNotIn(
+            '<script>alert(1)</script>',
+            html,
+        )
+
+    def test_task_instance_link(self):
+        mock_task = mock.Mock(dag_id='<a&1>', task_id='<b2>', execution_date=datetime(2017, 10, 12))
+        with self.app.test_request_context():
+            html = str(self.views.task_instance_link(
+                v=None, c=None, m=mock_task, p=None
+            ))
+
+        self.assertIn('%3Ca%261%3E', html)
+        self.assertIn('%3Cb2%3E', html)
+        self.assertNotIn('<a&1>', html)
+        self.assertNotIn('<b2>', html)
+
+    def test_dag_link(self):
+        mock_dag = mock.Mock(dag_id='<a&1>', execution_date=datetime(2017, 10, 12))
+        with self.app.test_request_context():
+            html = str(self.views.dag_link(
+                v=None, c=None, m=mock_dag, p=None
+            ))
+
+        self.assertIn('%3Ca%261%3E', html)
+        self.assertNotIn('<a&1>', html)
+
+    def test_dag_run_link(self):
+        mock_run = mock.Mock(dag_id='<a&1>', run_id='<b2>', execution_date=datetime(2017, 10, 12))
+        with self.app.test_request_context():
+            html = str(self.views.dag_run_link(
+                v=None, c=None, m=mock_run, p=None
+            ))
+
+        self.assertIn('%3Ca%261%3E', html)
+        self.assertIn('%3Cb2%3E', html)
+        self.assertNotIn('<a&1>', html)
+        self.assertNotIn('<b2>', html)
 
 
 if __name__ == '__main__':
