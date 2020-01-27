5c2253bf6a9b07ff931a2623aded22b3f7396aff
Cloud Composer Team <no-reply@google.com>
2020-01-27 14:14:51 +0100

    Remove check for explicit_defaults_for_timestamp during db initialization.
    (same as in Airflow 1.10.3: https://cloud-airflow.googlesource.com/composer-airflow/+/refs/heads/1.10.3/airflow/migrations/versions/d38e04c12aa2_add_serialized_dag_table.py)
    
    Change-Id: Ib50be0e580dd8afaaddef2a257d4aba54501bef0
    GitOrigin-RevId: 2e93a92fd1071869de0f1505e363fc434251bf35

--- airflow/migrations/versions/d38e04c12aa2_add_serialized_dag_table.py
+++ airflow/migrations/versions/d38e04c12aa2_add_serialized_dag_table.py
@@ -57,12 +57,6 @@ def upgrade():
 
     if conn.dialect.name == "mysql":
         conn.execute("SET time_zone = '+00:00'")
-        cur = conn.execute("SELECT @@explicit_defaults_for_timestamp")
-        res = cur.fetchall()
-        if res[0][0] == 0:
-            raise Exception(
-                "Global variable explicit_defaults_for_timestamp needs to be on (1) for mysql"
-            )
 
         op.alter_column(  # pylint: disable=no-member
             table_name="serialized_dag",
