34978e6dba2fd051ea5f28849eb8fbfbea9e7db8
Cloud Composer Team <no-reply@google.com>
2020-02-10 15:50:17 +0100

    Yet another fix for DAG run via REST API
    Previously I have cherry-picked this from master branch that handles dag
    folder location differently.
    
    Tested on my computer with:
    curl -X POST
    http://localhost:8080/api/experimental/dags/example_bash_operator/dag_runs
    -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d
    '{"conf":"{\"key\":\"value\"}"}'
    
    Change-Id: I98a132eb27bffc8781dc5903a31ae4ba84890d06
    GitOrigin-RevId: 453a5b05eda2d929565ef35182224ec67fc09502

--- airflow/api/common/experimental/trigger_dag.py
+++ airflow/api/common/experimental/trigger_dag.py
@@ -19,7 +19,7 @@
 
 import json
 
-from airflow.models import DagModel
+from airflow import settings
 
 from airflow.exceptions import DagRunAlreadyExists, DagNotFound
 from airflow.models import DagRun, DagBag
@@ -90,15 +90,11 @@ def trigger_dag(
         execution_date=None,
         replace_microseconds=True,
 ):
-    dag_model = DagModel.get_current(dag_id)
-    if dag_model is None:
-        raise DagNotFound("Dag id {} not found in DagModel".format(dag_id))
-
     def read_store_serialized_dags():
         from airflow.configuration import conf
         return conf.getboolean('core', 'store_serialized_dags')
     dagbag = DagBag(
-        dag_folder=dag_model.fileloc,
+        dag_folder=settings.DAGS_FOLDER,
         store_serialized_dags=read_store_serialized_dags()
     )
     dag_run = DagRun()
