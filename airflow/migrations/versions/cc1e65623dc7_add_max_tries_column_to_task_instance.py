a89369d303264a3875e5fa4946d2c102504aa465
Cloud Composer Team <no-reply@google.com>
2020-03-09 11:22:33 +0100

    Fix migrations by setting core.store_serialized_dags to false
    Those migrations run before serialized_dag table is present
    
    Change-Id: Ic81f27762747b88b5c15970646997e4971f79d1c
    GitOrigin-RevId: 2c7f42c448162eea50a540c0e63a033a0511ec80

--- airflow/migrations/versions/cc1e65623dc7_add_max_tries_column_to_task_instance.py
+++ airflow/migrations/versions/cc1e65623dc7_add_max_tries_column_to_task_instance.py
@@ -67,7 +67,8 @@ def upgrade():
         # Get current session
         sessionmaker = sa.orm.sessionmaker()
         session = sessionmaker(bind=connection)
-        dagbag = DagBag(settings.DAGS_FOLDER)
+        dagbag = DagBag(
+            dag_folder=settings.DAGS_FOLDER, store_serialized_dags=False)
         query = session.query(sa.func.count(TaskInstance.max_tries)).filter(
             TaskInstance.max_tries == -1
         )
@@ -105,7 +106,8 @@ def downgrade():
         connection = op.get_bind()
         sessionmaker = sa.orm.sessionmaker()
         session = sessionmaker(bind=connection)
-        dagbag = DagBag(settings.DAGS_FOLDER)
+        dagbag = DagBag(
+            dag_folder=settings.DAGS_FOLDER, store_serialized_dags=False)
         query = session.query(sa.func.count(TaskInstance.max_tries)).filter(
             TaskInstance.max_tries != -1
         )
