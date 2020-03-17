7f0da637c43a2eb3eb3d787630f93dd5b5219972
Cloud Composer Team <no-reply@google.com>
2020-03-17 17:34:04 +0100

    Add test for running DAG from UI when reading dag from db
    Change-Id: I253f128663dfd0cf8291fa0cfb0d8093a10cd810
    GitOrigin-RevId: 1b699ce8ac38b846963b0c60017b65b04d6cec45

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -929,6 +929,25 @@ class TestTriggerDag(unittest.TestCase):
         self.assertIsNotNone(run)
         self.assertIn("manual__", run.run_id)
 
+    def test_trigger_dag_button_serialized_dags(self):
+        with conf_vars(
+            {
+                ("core", "store_serialized_dags"): "True"
+            }
+        ):
+            test_dag_id = "example_bash_operator"
+            models.DagBag().get_dag(test_dag_id).sync_to_db()
+
+            DR = models.DagRun
+            self.session.query(DR).delete()
+            self.session.commit()
+
+            self.app.post('/admin/airflow/trigger?dag_id={}'.format(test_dag_id))
+
+            run = self.session.query(DR).filter(DR.dag_id == test_dag_id).first()
+            self.assertIsNotNone(run)
+            self.assertIn("manual__", run.run_id)
+
 
 class HelpersTest(unittest.TestCase):
     @classmethod
