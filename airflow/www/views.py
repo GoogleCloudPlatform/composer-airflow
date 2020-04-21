c73bcbcd146a156086d7771f92e10e633f0773be
Kaxil Naik <kaxilnaik@gmail.com>
2020-04-21 17:43:50 +0100

    BugFix: DAG trigger via UI error in RBAC UI (#8411)
    resolves https://github.com/apache/airflow/issues/8247
    
    connected to Internal bug
    
    Change-Id: Iabbaf5f8fb88c6ec1c9b3928354efbcb74ff58cc
    GitOrigin-RevId: cb7bb1ca23086ee36dcc6eb92ab1474eedc8d17a

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -1182,6 +1182,7 @@ class Airflow(BaseView):
 
         run_conf = {}
 
+        dag = dagbag.get_dag(dag_id)
         dag.create_dagrun(
             run_id=run_id,
             execution_date=execution_date,
