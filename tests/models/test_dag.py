00295d1284a0f41591a833944a9735538c745402
Cloud Composer Team <no-reply@google.com>
2020-05-24 11:06:19 +0200

    fixing tests/models/test_dag.py::est_sync_to_db test
    Change-Id: I0207a21696bd31d4938b5722fca65a5d8c704079
    GitOrigin-RevId: f2e8a856d768be0846b9f2170769b8727a6f7d80

--- tests/models/test_dag.py
+++ tests/models/test_dag.py
@@ -677,7 +677,8 @@ class DagTest(unittest.TestCase):
         self.assertEqual(set(orm_dag.owners.split(', ')), {'owner1', 'owner2'})
         self.assertEqual(orm_dag.last_scheduler_run, now)
         self.assertTrue(orm_dag.is_active)
-        self.assertIsNone(orm_dag.default_view)
+        self.assertIsNotNone(orm_dag.default_view)
+        self.assertEqual(orm_dag.get_default_view(), "tree")
         self.assertEqual(orm_dag.get_default_view(),
                          conf.get('webserver', 'dag_default_view').lower())
         self.assertEqual(orm_dag.safe_dag_id, 'dag')
