ca162a179267da01cb915897eab4fa0647391a24
Cloud Composer Team <no-reply@google.com>
2020-05-28 08:32:52 +0200

    Added repeating logic to reading env_var.json, as it can have problems with syncing to the local filesystem (and be missing even for up to 20s).
    Change-Id: Id5aeea53f9c94bab8ababa0402d1a80c3a5888c9
    GitOrigin-RevId: ee7477a2879836a9d0ef7ce98023d81c350686c4

--- tests/task/task_runner/test_standard_task_runner.py
+++ tests/task/task_runner/test_standard_task_runner.py
@@ -123,7 +123,8 @@ class TestStandardTaskRunner(unittest.TestCase):
         runner.start()
 
         # give the task some time to startup
-        time.sleep(3)
+        # Upped to 30 to wait for the retry logic when reading "env_var.json".
+        time.sleep(30)
 
         runner.terminate()
 
