020c890866d3e61b1827edbb449dc5809c8582e2
Cloud Composer Team <no-reply@google.com>
2020-06-15 13:45:14 +0200

    Do not call env var reloading with every airflow modules reload
    Reading env vars is called implicitly every time airflow modules are
    being reloaded (six.moves.reload_module(airflow).
    
    We should not call it unnecessesarilly as it takes time in case env
    variables are not being sent to docker and they are only being sent and
    used in web server.
    
    Change-Id: I4551176034284379fea4bef0f632ab8bd1fd854e
    GitOrigin-RevId: b0378ed2111db5348020df945c5ec64440c30ab8

--- airflow/www/__init__.py
+++ airflow/www/__init__.py
@@ -16,3 +16,35 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from datetime import datetime, timedelta
+import json
+import logging
+import os
+import time
+
+
+def __load__environment_variables__():
+    with open('/home/airflow/gcs/env_var.json', 'r') as env_var_json:
+        os.environ.update(json.load(env_var_json))
+    logging.info('Composer Environment Variables have been loaded.')
+
+
+def init_env_vars():
+    # Wait up to 25 seconds for env_var.json to sync
+    # (gsutil cp to the local filesystem)
+    timeout_at = datetime.now() + timedelta(seconds=25)
+
+    while datetime.now() < timeout_at:
+        try:
+            __load__environment_variables__()
+            break
+        except:
+            time.sleep(1)
+    else:
+        try:
+            __load__environment_variables__()
+        except:
+            logging.warning('Can\'t load Environment Variable overrides.',
+                            exc_info=True)
+            logging.warning('Using default Composer Environment Variables. Overrides '
+                            'have not been applied.')
