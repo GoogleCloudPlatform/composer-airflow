961e460bbea4c2c3b03cedcf8e392ed3b99f4939
Cloud Composer Team <no-reply@google.com>
2020-06-19 17:59:08 +0200

    Fix migrations with fileloc_hash of type varchar.
    In older versions of airflow in version composer-1.10.3 the
    fileloc_hash column is of type varchar not integer. In such
    case alter_column to BigInteger won't work. In this commit
    the migration is fixed. The column is being removed, recreated
    and recalculated.
    
    Internal bug
    
    Change-Id: I5a2ed2bafff73cbd78a7bb5424a5f1fd30eb8a41
    GitOrigin-RevId: afe77c552ce9844d2d559e06d8a73df295192ce1

--- airflow/migrations/versions/952da73b5eff_add_dag_code_table.py
+++ airflow/migrations/versions/952da73b5eff_add_dag_code_table.py
@@ -48,11 +48,20 @@ def upgrade():
     conn = op.get_bind()
     if conn.dialect.name != 'sqlite':
         op.drop_index('idx_fileloc_hash', 'serialized_dag')
-        op.alter_column(table_name='serialized_dag', column_name='fileloc_hash',
-                        type_=sa.BigInteger(), nullable=False)
+        op.drop_column(table_name='serialized_dag', column_name='fileloc_hash')
+        op.add_column(
+            table_name='serialized_dag',
+            column=sa.Column('fileloc_hash', sa.BigInteger(), nullable=False))
         op.create_index(   # pylint: disable=no-member
             'idx_fileloc_hash', 'serialized_dag', ['fileloc_hash'])
 
+    sessionmaker = sa.orm.sessionmaker()
+    session = sessionmaker(bind=conn)
+    serialized_dags = session.query(SerializedDagModel).all()
+    for dag in serialized_dags:
+        dag.fileloc_hash = DagCode.dag_fileloc_hash(dag.fileloc)
+        session.merge(dag)
+    session.commit()
 
 def downgrade():
     """Unapply add source code table"""
