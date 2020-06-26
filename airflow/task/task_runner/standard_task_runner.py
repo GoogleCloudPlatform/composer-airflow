7d689e4b154a36b2f945a73e2269541379136ff3
Cloud Composer Team <no-reply@google.com>
2020-06-26 22:16:58 +0200

    Fix logging issues from tasks.
    This should be cherry-picked until
    https://github.com/apache/airflow/pull/9363
    is included
    
    Change-Id: I4f6a7208eb4513261a6170563254a09fafeb1ddb
    GitOrigin-RevId: c1da7587f554f8db6a50ddbff85557d025d13a96

--- airflow/task/task_runner/standard_task_runner.py
+++ airflow/task/task_runner/standard_task_runner.py
@@ -25,7 +25,7 @@ from setproctitle import setproctitle
 from airflow.task.task_runner.base_task_runner import BaseTaskRunner
 from airflow.utils.helpers import reap_process_group
 
-CAN_FORK = hasattr(os, 'fork')
+CAN_FORK = False # hasattr(os, 'fork')
 
 
 class StandardTaskRunner(BaseTaskRunner):
