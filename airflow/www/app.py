0a42ed23c0b43d78568784c4deec7bc74b7eba93
Cloud Composer Team <no-reply@google.com>
2020-07-16 10:46:27 +0200

    Reading variables from json should only be attempted if the file exists.
    This should stop the warning about default values from showing up for
    worker pods, and now that we've added the .bkp file in the webserver
    the expectation is that at least this one will exist.
    
    Change-Id: Ic0b97fca6732c35633db50637b83c542946d127c
    GitOrigin-RevId: 6c2409e1b9467486819bc4b6765f9f20c1f2e26e

--- airflow/www/app.py
+++ airflow/www/app.py
@@ -46,15 +46,23 @@ from airflow.utils.net import get_hostname
 
 csrf = CSRFProtect()
 
-try:
-    with open('/home/airflow/gcs/env_var.json', 'r') as env_var_json:
-        os.environ.update(json.load(env_var_json))
-    logging.info('Composer Environment Variables have been loaded.')
-except:
-    logging.warning('Can\'t load Environment Variable overrides.',
-                  exc_info=True)
-    logging.warning('Using default Composer Environment Variables. Overrides '
-                'have not been applied.')
+if os.path.isfile('/home/airflow/gcs/env_var.json') \
+  or os.path.isfile('/home/airflow/gcs/env_var.json.bkp'):
+    try:
+        with open('/home/airflow/gcs/env_var.json', 'r') as env_var_json:
+            env_var_content = json.load(env_var_json)
+            os.environ.update(env_var_content)
+        with open('/home/airflow/gcs/env_var.json.bkp', 'w') as env_var_backup:
+            json.dump(env_var_content, env_var_backup)
+    except:
+        try:
+            with open('/home/airflow/gcs/env_var.json.bkp', 'r') as env_var_json:
+                env_var_content = json.load(env_var_json)
+                os.environ.update(env_var_content)
+            logging.info('Composer Environment Variables were loaded from backup.')
+        except:
+            logging.warning('Using default Composer Environment Variables. Overrides '
+                            'have not been applied.')
 
 if not conf.getboolean('core', 'unit_test_mode'):
     airflow.plugins_manager = six.moves.reload_module(airflow.plugins_manager)
