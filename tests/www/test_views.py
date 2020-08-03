4c0505bc15c8a61f8e605b22b1119e1cbd087d78
Cloud Composer Team <no-reply@google.com>
2020-08-03 17:52:45 +0200

    Internal Change
    GitOrigin-RevId: 4ca088ad9dd3c71bbce748e137833763f377bc4f

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -1229,7 +1229,6 @@ class TestDagModelView(unittest.TestCase):
             self.EDIT_URL,
             data={
                 "fileloc": "/etc/passwd",
-                "description": "Set in tests",
             },
             follow_redirects=True,
         )
@@ -1239,7 +1238,6 @@ class TestDagModelView(unittest.TestCase):
         dm = session.query(DM).filter(DM.dag_id == 'example_bash_operator').one()
         session.close()
 
-        self.assertEqual(dm.description, "Set in tests")
         self.assertNotEqual(dm.fileloc, "/etc/passwd", "Disabled fields shouldn't be updated")
 
 
