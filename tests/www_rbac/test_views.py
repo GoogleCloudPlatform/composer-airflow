7a6d8c6b343074be48d225092b06cc21085c43b2
Cloud Composer Team <no-reply@google.com>
2020-10-02 17:07:37 +0200

    Adjust Airflow RBAC UI health check path.
    Composer expects the health check endpoint at /_ah/health instead of
    /health.
    
    Internal bug
    
    Change-Id: I160f6c7dbc9264d299cf8abe29f7a96eda0ab5a4
    GitOrigin-RevId: 5bef43eba08f5dd0f7644716ad6a7eb7b4e4744d

--- tests/www_rbac/test_views.py
+++ tests/www_rbac/test_views.py
@@ -355,7 +355,7 @@ class TestMountPoint(unittest.TestCase):
 
     def test_mount(self):
         # Test an endpoint that doesn't need auth!
-        resp = self.client.get('/test/health')
+        resp = self.client.get('/test/_ah/health')
         self.assertEqual(resp.status_code, 200)
         self.assertIn(b"healthy", resp.data)
 
@@ -433,7 +433,7 @@ class TestAirflowBaseViews(TestBase):
                                  latest_heartbeat=last_scheduler_heartbeat_for_testing_1))
         self.session.commit()
 
-        resp_json = json.loads(self.client.get('health', follow_redirects=True).data.decode('utf-8'))
+        resp_json = json.loads(self.client.get('_ah/health', follow_redirects=True).data.decode('utf-8'))
 
         self.assertEqual('healthy', resp_json['metadatabase']['status'])
         self.assertEqual('healthy', resp_json['scheduler']['status'])
@@ -458,7 +458,7 @@ class TestAirflowBaseViews(TestBase):
                                  latest_heartbeat=last_scheduler_heartbeat_for_testing_2))
         self.session.commit()
 
-        resp_json = json.loads(self.client.get('health', follow_redirects=True).data.decode('utf-8'))
+        resp_json = json.loads(self.client.get('_ah/health', follow_redirects=True).data.decode('utf-8'))
 
         self.assertEqual('healthy', resp_json['metadatabase']['status'])
         self.assertEqual('unhealthy', resp_json['scheduler']['status'])
@@ -479,7 +479,7 @@ class TestAirflowBaseViews(TestBase):
             delete()
         self.session.commit()
 
-        resp_json = json.loads(self.client.get('health', follow_redirects=True).data.decode('utf-8'))
+        resp_json = json.loads(self.client.get('_ah/health', follow_redirects=True).data.decode('utf-8'))
 
         self.assertEqual('healthy', resp_json['metadatabase']['status'])
         self.assertEqual('unhealthy', resp_json['scheduler']['status'])
