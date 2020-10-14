11ae96c5c2c703a6cc56b3abc4836f5efbed57b3
Jarek Potiuk <jarekpotiuk@google.com>
2020-10-14 09:07:23 +0000

    Import required patches from 1.9.0
    Change-Id: I35de4dc54ffd7dacfddadd8bb31f7be5d498e79d
    GitOrigin-RevId: 67d7a6c04e9ab144bb6ee7147e1d44921dbf1c4a

--- airflow/utils/helpers.py
+++ airflow/utils/helpers.py
@@ -356,6 +356,86 @@ def parse_template_string(template_string):
     else:
         return template_string, None
 
+def kill_using_shell(logger, pid, signal=signal.SIGTERM):
+    try:
+        process = psutil.Process(pid)
+        # Use sudo only when necessary - consider SubDagOperator and SequentialExecutor case.
+        if process.username() != getpass.getuser():
+            args = ["sudo", "kill", "-{}".format(int(signal)), str(pid)]
+        else:
+            args = ["kill", "-{}".format(int(signal)), str(pid)]
+        # PID may not exist and return a non-zero error code
+        logger.error(subprocess.check_output(args))
+        logger.info("Killed process {} with signal {}".format(pid, signal))
+        return True
+    except psutil.NoSuchProcess as e:
+        logger.warning("Process {} no longer exists".format(pid))
+        return False
+    except subprocess.CalledProcessError as e:
+        logger.warning("Failed to kill process {} with signal {}. Output: {}"
+                       .format(pid, signal, e.output))
+        return False
+
+def kill_process_tree(logger, pid, timeout=DEFAULT_TIME_TO_WAIT_AFTER_SIGTERM):
+    """
+    TODO(saguziel): also kill the root process after killing descendants
+
+    Kills the process's descendants. Kills using the `kill`
+    shell command so that it can change users. Note: killing via PIDs
+    has the potential to the wrong process if the process dies and the
+    PID gets recycled in a narrow time window.
+
+    :param logger: logger
+    :type logger: logging.Logger
+    """
+    try:
+        root_process = psutil.Process(pid)
+    except psutil.NoSuchProcess:
+        logger.warning("PID: {} does not exist".format(pid))
+        return
+
+    # Check child processes to reduce cases where a child process died but
+    # the PID got reused.
+    descendant_processes = [x for x in root_process.children(recursive=True)
+                            if x.is_running()]
+
+    if len(descendant_processes) != 0:
+        logger.info("Terminating descendant processes of {} PID: {}"
+                    .format(root_process.cmdline(),
+                            root_process.pid))
+        temp_processes = descendant_processes[:]
+        for descendant in temp_processes:
+            logger.info("Terminating descendant process {} PID: {}"
+                        .format(descendant.cmdline(), descendant.pid))
+            if not kill_using_shell(logger, descendant.pid, signal.SIGTERM):
+                descendant_processes.remove(descendant)
+
+        logger.info("Waiting up to {}s for processes to exit..."
+                    .format(timeout))
+        try:
+            psutil.wait_procs(descendant_processes, timeout)
+            logger.info("Done waiting")
+        except psutil.TimeoutExpired:
+            logger.warning("Ran out of time while waiting for "
+                           "processes to exit")
+        # Then SIGKILL
+        descendant_processes = [x for x in root_process.children(recursive=True)
+                                if x.is_running()]
+
+        if len(descendant_processes) > 0:
+            temp_processes = descendant_processes[:]
+            for descendant in temp_processes:
+                logger.info("Killing descendant process {} PID: {}"
+                            .format(descendant.cmdline(), descendant.pid))
+                if not kill_using_shell(logger, descendant.pid, signal.SIGKILL):
+                    descendant_processes.remove(descendant)
+                else:
+                    descendant.wait()
+            logger.info("Killed all descendant processes of {} PID: {}"
+                        .format(root_process.cmdline(),
+                                root_process.pid))
+    else:
+        logger.debug("There are no descendant processes to kill")
 
 class AirflowImporter(object):
     """
