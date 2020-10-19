e34f14451e5fc0edd606afdcd65c413387342e86
Cloud Composer Team <no-reply@google.com>
2020-10-19 17:20:18 +0000

    Composer dependencies patch
    Changes:
    - add pre-commit configuration file
    - add suffix +composer to version
    - set Composer pypi dependencies
    - adjust some tests
    
    Change-Id: Idb198128ad0e6ccc3eb1d6a88945826f0c5cce65
    GitOrigin-RevId: 0d586c67ac9844f9d615a1c161a8307f29879c30

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ PY39 = sys.version_info >= (3, 9)
 
 logger = logging.getLogger(__name__)
 
-version = '2.3.3'
+version = '2.3.3+composer'
 
 my_dir = dirname(__file__)
 
@@ -570,6 +570,21 @@ yandex = [
 zendesk = [
     'zenpy>=2.0.24',
 ]
+composer_additional = [
+    "crcmod<2.0",
+    "google-apitools",
+    "google-cloud-aiplatform",
+    "google-cloud-datastore",
+    "google-cloud-pubsublite<1.0.0",
+    "keyrings.google-artifactregistry-auth",
+    "pip<20.3.0",
+    "pipdeptree",
+    "tensorflow==2.2.0",
+]
+
+composer = (
+    mysql + password + postgres + celery + redis + statsd + virtualenv + composer_additional + apache_beam
+)
 # End dependencies group
 
 # Mypy 0.900 and above ships only with stubs from stdlib so if we need other stubs, we need to install them
@@ -754,6 +769,7 @@ CORE_EXTRAS_REQUIREMENTS: Dict[str, List[str]] = {
     'celery': celery,  # also has provider, but it extends the core with the CeleryExecutor
     'cgroups': cgroups,
     'cncf.kubernetes': kubernetes,  # also has provider, but it extends the core with the KubernetesExecutor
+    'composer': composer,
     'dask': dask,
     'deprecated_api': deprecated_api,
     'github_enterprise': flask_appbuilder_oauth,
@@ -1065,7 +1081,9 @@ def replace_extra_requirement_with_provider_packages(extra: str, providers: List
         ]
 
 
-def add_provider_packages_to_extra_requirements(extra: str, providers: List[str]) -> None:
+def add_provider_packages_to_extra_requirements(
+    extra: str, providers: List[str], constraints: Dict[str, str] = None
+) -> None:
     """
     Adds provider packages as requirements to extra. This is used to add provider packages as requirements
     to the "bulk" kind of extras. Those bulk extras do not have the detailed 'extra' requirements as
@@ -1073,9 +1091,17 @@ def add_provider_packages_to_extra_requirements(extra: str, providers: List[str]
 
     :param extra: Name of the extra to add providers to
     :param providers: list of provider ids
+    :param constraints: constraints for providers
     """
+    if constraints is None:
+        constraints = {}
     EXTRAS_REQUIREMENTS[extra].extend(
-        [get_provider_package_from_package_id(package_name) for package_name in providers]
+        [
+            '{}{}'.format(
+                get_provider_package_from_package_id(package_name), constraints.get(package_name, '')
+            )
+            for package_name in providers
+        ]
     )
 
 
@@ -1099,6 +1125,28 @@ def add_all_provider_packages() -> None:
         "devel_hadoop", ["apache.hdfs", "apache.hive", "presto", "trino"]
     )
     add_all_deprecated_provider_packages()
+    add_provider_packages_to_extra_requirements(
+        "composer",
+        [
+            "apache.beam",
+            "cncf.kubernetes",
+            "google",
+            "hashicorp",
+            "http",
+            "mysql",
+            "postgres",
+            "sendgrid",
+            "ssh",
+        ],
+        {
+            # TODO: (should be removed in Airflow 2.3.4+) generic code for SQL operators and hooks was moved
+            # to the new common-sql provider package which cause a issue in the tests for core operators
+            "google": "==8.1.0",
+            "mysql": "==3.0.0",
+            "postgres": "==5.0.0",
+            "sqlite": "==3.0.0",
+        },
+    )
 
 
 class Develop(develop_orig):
