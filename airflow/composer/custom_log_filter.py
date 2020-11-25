ff72a3a6c3f8fb8be801e6dff250ea5a2a9773ae
Cloud Composer Team <no-reply@google.com>
2020-11-25 16:02:00 +0000

    Composer logging/metrics patch
    Changes:
    - provide custom GCS task handler
    - write task logs to stdout for fluentd to expose in Cloud Logging
    - write DAG processor manager logs to stdout for fluentd to expose in Cloud Logging
    - write custom Composer metrics
    - implement custom Composer log filter
    - use same log format for Celery logs as for all other logs
    
    Change-Id: Ie6ca8e8a544dbd661bc74db38b2cc419144bb9a2
    GitOrigin-RevId: 91881bd34482cd4a6a24797377a11b6840d77ca6

---
+++ airflow/composer/custom_log_filter.py
@@ -0,0 +1,94 @@
+#
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Custom log filter for Cloud Composer."""
+import logging
+import os
+import re
+import warnings
+
+PROVIDERS_HOOK_MISSING_ATTRIBUTE_WARNING_RE = re.compile(
+    r"The '<class 'airflow.providers[a-zA-Z\.]*Hook'>' is missing [a-z_]* attribute and cannot be registered"
+)
+
+# In Composer default Airflow configuration properties we continue to use
+# [core]dag_concurrency property, because if we update it to the new Airflow property
+# it will take precedence over custom [core]dag_concurrency set by customer.
+# Here, we silence this warning message as this message is expected and doesn't
+# require any action items.
+warnings.filterwarnings(
+    'ignore', r'.*The dag_concurrency option in \[core\] has been renamed to max_active_tasks_per_dag.*'
+)
+
+
+def _is_redis_warning(record):
+    """Method that detects using Redis as result backend warning."""
+    return record.getMessage().startswith('You have configured a result_backend of redis://')
+
+
+def _is_stats_client_warning(record):
+    """Method that detects stats client is not configured warning."""
+    return record.getMessage().startswith('Could not configure StatsClient: ')
+
+
+def _is_refused_to_delete_permission_view_warning(record):
+    """Method that detects refused to delete permission view warning."""
+    return record.getMessage().startswith('Refused to delete permission view, assoc with role exists ')
+
+
+def _is_no_user_yet_created_warning(record):
+    """Method that detects no user yet created warning."""
+    return record.getMessage() == 'No user yet created, use flask fab command to do it.'
+
+
+def _is_providers_hook_missing_attribute_warning(record):
+    """Method that detects missing attribute warning for provider hooks."""
+    return PROVIDERS_HOOK_MISSING_ATTRIBUTE_WARNING_RE.match(record.getMessage())
+
+
+class ComposerFilter(logging.Filter):
+    """Custom Composer log filter."""
+
+    def filter(self, record):
+        # The concern with running Redis backend is that celery task messages may get
+        # lost across Redis restarts. Composer has provisioned Redis service using
+        # StatefulSet and saves a snapshot every 60 seconds to a persistent disk.
+        # So it's not a concern to use the Redis backend in Cloud Composer.
+        # From https://groups.google.com/g/cloud-composer-discuss/c/8SY2NdjjOS4
+        if _is_redis_warning(record):
+            return False
+
+        # Webserver doesn't have access to statsd host, and it doesn't need because it doesn't
+        # send any metrics, therefore we can safely silent this message for webserver.
+        if _is_stats_client_warning(record) and os.environ.get('AIRFLOW_WEBSERVER', None) == 'True':
+            return False
+
+        # According to https://github.com/apache/airflow/issues/10331#issuecomment-758108624
+        # these warning messages are harmless and can be ignored.
+        if _is_refused_to_delete_permission_view_warning(record):
+            return False
+
+        # This warning is printed on start up of webserver in case no users are yet registered
+        # in Airflow RBAC. This message can be silent as it is not useful.
+        if _is_no_user_yet_created_warning(record):
+            return False
+
+        # Warning about missing attribute for hook doesn't mean this hook is not usable,
+        # it means that this hook will not be available e.g. on "Connections" page in UI.
+        # In case of custom user hooks this warning is useful, in case of provider hooks
+        # this warning is useless for Airflow user because they are not able to fix it.
+        if _is_providers_hook_missing_attribute_warning(record):
+            return False
+
+        return True
---
+++ tests/composer/test_dag_processor_manager_formatter.py
@@ -0,0 +1,48 @@
+#
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import contextlib
+import importlib
+import io
+import logging
+import unittest
+from unittest.mock import patch
+
+from airflow.config_templates import airflow_local_settings
+from airflow.logging_config import configure_logging
+
+
+class TestDagProcessorManagerFormatter(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with patch.dict("os.environ", {"CONFIG_PROCESSOR_MANAGER_LOGGER": "True"}):
+            importlib.reload(airflow_local_settings)
+            configure_logging()
+
+    def test_single_line(self):
+        logger = logging.getLogger("airflow.processor_manager")
+        message = "Test message"
+        with contextlib.redirect_stdout(io.StringIO()) as temp_stdout:
+            logger.warning(message)
+            self.assertRegex(temp_stdout.getvalue(), "DAG_PROCESSOR_MANAGER_LOG:.*Test message")
+
+    def test_multi_line(self):
+        logger = logging.getLogger("airflow.processor_manager")
+        message = "\n".join(["line-1", "line-2", "last-line"])
+        with contextlib.redirect_stdout(io.StringIO()) as temp_stdout:
+            logger.warning(message)
+            self.assertRegex(temp_stdout.getvalue(), "DAG_PROCESSOR_MANAGER_LOG:.*line-1")
+            self.assertIn("DAG_PROCESSOR_MANAGER_LOG:line-2", temp_stdout.getvalue())
+            self.assertIn("DAG_PROCESSOR_MANAGER_LOG:last-line", temp_stdout.getvalue())
