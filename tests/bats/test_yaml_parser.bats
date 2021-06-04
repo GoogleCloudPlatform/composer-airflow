apache/spark

[AIRFLOW-5704] Improve Kind Kubernetes scripts for local testing (#6516)
* Fixed problem that Kubernetes tests were testing latest master
  rather than what came from the local sources.
* Kind (Kubernetes in Dcocker) is run in the same Docker as Breeze env
* Moved Kubernetes scripts to 'in_container' dir where they belong now
* Kubernetes cluster is reused until it is stopped
* Kubernetes image is build from image in docker already + mounted sources
* Kubectl version name is corrected in the Dockerfile
* KUBERNETES_VERSION can now be used to select Kubernetes version
* Running kubernetes scripts is now easy in Breeze
* We can start/recreate/stop cluster using  --<ACTION>-kind-cluster
* Instructions on how to run Kubernetes tests are updated
* The old "bare" environment is replaced by --no-deps switch

tests/bats/test_yaml_parser.bats
@@ -36,35 +36,38 @@ services_postgres_ports_1="${POSTGRES_HOST_PORT}:5432"
 services_airflow-testing_volumes_1="../../.bash_aliases:/root/.bash_aliases:cached"
 services_airflow-testing_volumes_2="../../.bash_history:/root/.bash_history:cached"
 services_airflow-testing_volumes_3="../../.coveragerc:/opt/airflow/.coveragerc:cached"
-services_airflow-testing_volumes_4="../../.flake8:/opt/airflow/.flake8:cached"
-services_airflow-testing_volumes_5="../../.github:/opt/airflow/.github:cached"
-services_airflow-testing_volumes_6="../../.inputrc:/root/.inputrc:cached"
-services_airflow-testing_volumes_7="../../.rat-excludes:/opt/airflow/.rat-excludes:cached"
-services_airflow-testing_volumes_8="../../CHANGELOG.txt:/opt/airflow/CHANGELOG:cached"
-services_airflow-testing_volumes_9="../../LICENSE:/opt/airflow/LICENSE:cached"
-services_airflow-testing_volumes_10="../../MANIFEST.in:/opt/airflow/MANIFEST.in:cached"
-services_airflow-testing_volumes_11="../../NOTICE:/opt/airflow/NOTICE:cached"
-services_airflow-testing_volumes_12="../../airflow:/opt/airflow/airflow:cached"
-services_airflow-testing_volumes_13="../../common:/opt/airflow/common:cached"
-services_airflow-testing_volumes_14="../../dags:/opt/airflow/dags:cached"
-services_airflow-testing_volumes_15="../../dev:/opt/airflow/dev:cached"
-services_airflow-testing_volumes_16="../../docs:/opt/airflow/docs:cached"
-services_airflow-testing_volumes_17="../../files:/files:cached"
-services_airflow-testing_volumes_18="../../hooks:/opt/airflow/hooks:cached"
-services_airflow-testing_volumes_19="../../logs:/root/airflow/logs:cached"
-services_airflow-testing_volumes_20="../../pylintrc:/opt/airflow/pylintrc:cached"
-services_airflow-testing_volumes_21="../../pytest.ini:/opt/airflow/pytest.ini:cached"
-services_airflow-testing_volumes_22="../../scripts:/opt/airflow/scripts:cached"
-services_airflow-testing_volumes_23="../../setup.cfg:/opt/airflow/setup.cfg:cached"
-services_airflow-testing_volumes_24="../../setup.py:/opt/airflow/setup.py:cached"
-services_airflow-testing_volumes_25="../../tests:/opt/airflow/tests:cached"
-services_airflow-testing_volumes_26="../../tmp:/opt/airflow/tmp:cached"
+services_airflow-testing_volumes_4="../../.dockerignore:/opt/airflow/.dockerignore:cached"
+services_airflow-testing_volumes_5="../../.flake8:/opt/airflow/.flake8:cached"
+services_airflow-testing_volumes_6="../../.github:/opt/airflow/.github:cached"
+services_airflow-testing_volumes_7="../../.inputrc:/root/.inputrc:cached"
+services_airflow-testing_volumes_8="../../.kube:/root/.kube:cached"
+services_airflow-testing_volumes_9="../../.rat-excludes:/opt/airflow/.rat-excludes:cached"
+services_airflow-testing_volumes_10="../../CHANGELOG.txt:/opt/airflow/CHANGELOG:cached"
+services_airflow-testing_volumes_11="../../Dockerfile:/opt/airflow/Dockerfile:cached"
+services_airflow-testing_volumes_12="../../LICENSE:/opt/airflow/LICENSE:cached"
+services_airflow-testing_volumes_13="../../MANIFEST.in:/opt/airflow/MANIFEST.in:cached"
+services_airflow-testing_volumes_14="../../NOTICE:/opt/airflow/NOTICE:cached"
+services_airflow-testing_volumes_15="../../airflow:/opt/airflow/airflow:cached"
+services_airflow-testing_volumes_16="../../common:/opt/airflow/common:cached"
+services_airflow-testing_volumes_17="../../dags:/opt/airflow/dags:cached"
+services_airflow-testing_volumes_18="../../dev:/opt/airflow/dev:cached"
+services_airflow-testing_volumes_19="../../docs:/opt/airflow/docs:cached"
+services_airflow-testing_volumes_20="../../files:/files:cached"
+services_airflow-testing_volumes_21="../../hooks:/opt/airflow/hooks:cached"
+services_airflow-testing_volumes_22="../../logs:/root/airflow/logs:cached"
+services_airflow-testing_volumes_23="../../pylintrc:/opt/airflow/pylintrc:cached"
+services_airflow-testing_volumes_24="../../pytest.ini:/opt/airflow/pytest.ini:cached"
+services_airflow-testing_volumes_25="../../scripts:/opt/airflow/scripts:cached"
+services_airflow-testing_volumes_26="../../scripts/ci/in_container/entrypoint_ci.sh:/entrypoint_ci.sh:cached"
+services_airflow-testing_volumes_27="../../setup.cfg:/opt/airflow/setup.cfg:cached"
+services_airflow-testing_volumes_28="../../setup.py:/opt/airflow/setup.py:cached"
+services_airflow-testing_volumes_29="../../tests:/opt/airflow/tests:cached"
+services_airflow-testing_volumes_30="../../tmp:/opt/airflow/tmp:cached"
 services_airflow-testing_environment_1="HOST_USER_ID"
 services_airflow-testing_environment_2="HOST_GROUP_ID"
 services_airflow-testing_environment_3="PYTHONDONTWRITEBYTECODE"
 services_airflow-testing_ports_1="${WEBSERVER_HOST_PORT}:8080"
 EOF
-
 }
 
 
@@ -79,15 +82,21 @@ ${AIRFLOW_SOURCES}/.bash_history:/root/.bash_history:cached
 -v
 ${AIRFLOW_SOURCES}/.coveragerc:/opt/airflow/.coveragerc:cached
 -v
+${AIRFLOW_SOURCES}/.dockerignore:/opt/airflow/.dockerignore:cached
+-v
 ${AIRFLOW_SOURCES}/.flake8:/opt/airflow/.flake8:cached
 -v
 ${AIRFLOW_SOURCES}/.github:/opt/airflow/.github:cached
 -v
 ${AIRFLOW_SOURCES}/.inputrc:/root/.inputrc:cached
 -v
+${AIRFLOW_SOURCES}/.kube:/root/.kube:cached
+-v
 ${AIRFLOW_SOURCES}/.rat-excludes:/opt/airflow/.rat-excludes:cached
 -v
 ${AIRFLOW_SOURCES}/CHANGELOG.txt:/opt/airflow/CHANGELOG:cached
+-v
+${AIRFLOW_SOURCES}/Dockerfile:/opt/airflow/Dockerfile:cached
 -v
 ${AIRFLOW_SOURCES}/LICENSE:/opt/airflow/LICENSE:cached
 -v
@@ -116,6 +125,8 @@ ${AIRFLOW_SOURCES}/pylintrc:/opt/airflow/pylintrc:cached
 ${AIRFLOW_SOURCES}/pytest.ini:/opt/airflow/pytest.ini:cached
 -v
 ${AIRFLOW_SOURCES}/scripts:/opt/airflow/scripts:cached
+-v
+${AIRFLOW_SOURCES}/scripts/ci/in_container/entrypoint_ci.sh:/entrypoint_ci.sh:cached
 -v
 ${AIRFLOW_SOURCES}/setup.cfg:/opt/airflow/setup.cfg:cached
 -v

GoogleCloudPlatform/composer-airflow

(cherry picked from commit 73403cc8f430b540db492afc5ca3d0664c0ca98b)
GitOrigin-RevId: c7c5aa3ca7f615531b2c8e7a95def5eca86373da

tests/bats/test_yaml_parser.bats
@@ -36,34 +36,37 @@ services_postgres_ports_1="${POSTGRES_HOST_PORT}:5432"
 services_airflow-testing_volumes_1="../../.bash_aliases:/root/.bash_aliases:cached"
 services_airflow-testing_volumes_2="../../.bash_history:/root/.bash_history:cached"
 services_airflow-testing_volumes_3="../../.coveragerc:/opt/airflow/.coveragerc:cached"
-services_airflow-testing_volumes_4="../../.flake8:/opt/airflow/.flake8:cached"
-services_airflow-testing_volumes_5="../../.github:/opt/airflow/.github:cached"
-services_airflow-testing_volumes_6="../../.inputrc:/root/.inputrc:cached"
-services_airflow-testing_volumes_7="../../.rat-excludes:/opt/airflow/.rat-excludes:cached"
-services_airflow-testing_volumes_8="../../CHANGELOG.txt:/opt/airflow/CHANGELOG:cached"
-services_airflow-testing_volumes_9="../../LICENSE:/opt/airflow/LICENSE:cached"
-services_airflow-testing_volumes_10="../../MANIFEST.in:/opt/airflow/MANIFEST.in:cached"
-services_airflow-testing_volumes_11="../../NOTICE:/opt/airflow/NOTICE:cached"
-services_airflow-testing_volumes_12="../../airflow:/opt/airflow/airflow:cached"
-services_airflow-testing_volumes_13="../../common:/opt/airflow/common:cached"
-services_airflow-testing_volumes_14="../../dags:/opt/airflow/dags:cached"
-services_airflow-testing_volumes_15="../../dev:/opt/airflow/dev:cached"
-services_airflow-testing_volumes_16="../../docs:/opt/airflow/docs:cached"
-services_airflow-testing_volumes_17="../../files:/files:cached"
-services_airflow-testing_volumes_18="../../hooks:/opt/airflow/hooks:cached"
-services_airflow-testing_volumes_19="../../logs:/root/airflow/logs:cached"
-services_airflow-testing_volumes_20="../../pytest.ini:/opt/airflow/pytest.ini:cached"
-services_airflow-testing_volumes_21="../../scripts:/opt/airflow/scripts:cached"
-services_airflow-testing_volumes_22="../../setup.cfg:/opt/airflow/setup.cfg:cached"
-services_airflow-testing_volumes_23="../../setup.py:/opt/airflow/setup.py:cached"
-services_airflow-testing_volumes_24="../../tests:/opt/airflow/tests:cached"
-services_airflow-testing_volumes_25="../../tmp:/opt/airflow/tmp:cached"
+services_airflow-testing_volumes_4="../../.dockerignore:/opt/airflow/.dockerignore:cached"
+services_airflow-testing_volumes_5="../../.flake8:/opt/airflow/.flake8:cached"
+services_airflow-testing_volumes_6="../../.github:/opt/airflow/.github:cached"
+services_airflow-testing_volumes_7="../../.inputrc:/root/.inputrc:cached"
+services_airflow-testing_volumes_8="../../.kube:/root/.kube:cached"
+services_airflow-testing_volumes_9="../../.rat-excludes:/opt/airflow/.rat-excludes:cached"
+services_airflow-testing_volumes_10="../../CHANGELOG.txt:/opt/airflow/CHANGELOG:cached"
+services_airflow-testing_volumes_11="../../Dockerfile:/opt/airflow/Dockerfile:cached"
+services_airflow-testing_volumes_12="../../LICENSE:/opt/airflow/LICENSE:cached"
+services_airflow-testing_volumes_13="../../MANIFEST.in:/opt/airflow/MANIFEST.in:cached"
+services_airflow-testing_volumes_14="../../NOTICE:/opt/airflow/NOTICE:cached"
+services_airflow-testing_volumes_15="../../airflow:/opt/airflow/airflow:cached"
+services_airflow-testing_volumes_16="../../common:/opt/airflow/common:cached"
+services_airflow-testing_volumes_17="../../dags:/opt/airflow/dags:cached"
+services_airflow-testing_volumes_18="../../dev:/opt/airflow/dev:cached"
+services_airflow-testing_volumes_19="../../docs:/opt/airflow/docs:cached"
+services_airflow-testing_volumes_20="../../files:/files:cached"
+services_airflow-testing_volumes_21="../../hooks:/opt/airflow/hooks:cached"
+services_airflow-testing_volumes_22="../../logs:/root/airflow/logs:cached"
+services_airflow-testing_volumes_23="../../pytest.ini:/opt/airflow/pytest.ini:cached"
+services_airflow-testing_volumes_24="../../scripts:/opt/airflow/scripts:cached"
+services_airflow-testing_volumes_25="../../scripts/ci/in_container/entrypoint_ci.sh:/entrypoint_ci.sh:cached"
+services_airflow-testing_volumes_26="../../setup.cfg:/opt/airflow/setup.cfg:cached"
+services_airflow-testing_volumes_27="../../setup.py:/opt/airflow/setup.py:cached"
+services_airflow-testing_volumes_28="../../tests:/opt/airflow/tests:cached"
+services_airflow-testing_volumes_29="../../tmp:/opt/airflow/tmp:cached"
 services_airflow-testing_environment_1="HOST_USER_ID"
 services_airflow-testing_environment_2="HOST_GROUP_ID"
 services_airflow-testing_environment_3="PYTHONDONTWRITEBYTECODE"
 services_airflow-testing_ports_1="${WEBSERVER_HOST_PORT}:8080"
 EOF
-
 }
 
 
@@ -78,15 +81,21 @@ ${AIRFLOW_SOURCES}/.bash_history:/root/.bash_history:cached
 -v
 ${AIRFLOW_SOURCES}/.coveragerc:/opt/airflow/.coveragerc:cached
 -v
+${AIRFLOW_SOURCES}/.dockerignore:/opt/airflow/.dockerignore:cached
+-v
 ${AIRFLOW_SOURCES}/.flake8:/opt/airflow/.flake8:cached
 -v
 ${AIRFLOW_SOURCES}/.github:/opt/airflow/.github:cached
 -v
 ${AIRFLOW_SOURCES}/.inputrc:/root/.inputrc:cached
 -v
+${AIRFLOW_SOURCES}/.kube:/root/.kube:cached
+-v
 ${AIRFLOW_SOURCES}/.rat-excludes:/opt/airflow/.rat-excludes:cached
 -v
 ${AIRFLOW_SOURCES}/CHANGELOG.txt:/opt/airflow/CHANGELOG:cached
+-v
+${AIRFLOW_SOURCES}/Dockerfile:/opt/airflow/Dockerfile:cached
 -v
 ${AIRFLOW_SOURCES}/LICENSE:/opt/airflow/LICENSE:cached
 -v
@@ -113,6 +122,8 @@ ${AIRFLOW_SOURCES}/logs:/root/airflow/logs:cached
 ${AIRFLOW_SOURCES}/pytest.ini:/opt/airflow/pytest.ini:cached
 -v
 ${AIRFLOW_SOURCES}/scripts:/opt/airflow/scripts:cached
+-v
+${AIRFLOW_SOURCES}/scripts/ci/in_container/entrypoint_ci.sh:/entrypoint_ci.sh:cached
 -v
 ${AIRFLOW_SOURCES}/setup.cfg:/opt/airflow/setup.cfg:cached
 -v