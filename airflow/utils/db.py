apache/spark

[AIRFLOW-6564] Additional diagnostics information on CI check failure (#7172)

airflow/utils/db.py
@@ -501,3 +501,13 @@ def resetdb():
     Base.metadata.drop_all(connection)  # pylint: disable=no-member
 
     initdb()
+
+
+@provide_session
+def check(session=None):
+    """
+    Checks if the database works.
+    :param session: session of the sqlalchemy
+    """
+    session.execute('select 1 as is_alive;')
+    log.info("Connection successful.")

GoogleCloudPlatform/composer-airflow

(cherry picked from commit 417feda5eba6cc63c01a9a04a7b48bc24f63a3d1)
GitOrigin-RevId: c71f6edcbc416b6d95950a4a0dfa33165c3ade68

airflow/utils/db.py
@@ -408,3 +408,13 @@ def resetdb(rbac):
     Base.metadata.drop_all(connection)
 
     initdb(rbac)
+
+
+@provide_session
+def checkdb(session=None):
+    """
+    Checks if the database works.
+    :param session: session of the sqlalchemy
+    """
+    session.execute('select 1 as is_alive;')
+    log.info("Connection successful.")