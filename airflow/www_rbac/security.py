apache/spark

[AIRFLOW-7048] Allow user to chose timezone to use in UI (#8046)
Co-authored-by: Sam Black <samblackk@users.noreply.github.com>

airflow/www/security.py
@@ -27,6 +27,7 @@ from airflow.exceptions import AirflowException
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.session import provide_session
 from airflow.www.app import appbuilder
+from airflow.www.utils import CustomSQLAInterface
 
 EXISTING_ROLES = {
     'Admin',
@@ -177,6 +178,20 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             'vms': VIEWER_VMS | DAG_VMS | USER_VMS | OP_VMS,
         },
     ]
+
+    def __init__(self, appbuilder):
+        super().__init__(appbuilder)
+
+        # Go and fix up the SQLAInterface used from the stock one to our subclass.
+        # This is needed to support the "hack" where we had to edit
+        # FieldConverter.conversion_table in place in airflow.www.utils
+        for attr in dir(self):
+            if not attr.endswith('view'):
+                continue
+            view = getattr(self, attr, None)
+            if not view or not getattr(view, 'datamodel', None):
+                continue
+            view.datamodel = CustomSQLAInterface(view.datamodel.obj)
 
     def init_role(self, role_name, role_vms, role_perms):
         """

airflow/www/utils.py
@@ -24,6 +24,7 @@ import flask_appbuilder.models.sqla.filters as fab_sqlafilters
 import markdown
 import sqlalchemy as sqla
 from flask import Markup, Response, request, url_for
+from flask_appbuilder.forms import DateTimeField, FieldConverter
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from pygments import highlight, lexers
 from pygments.formatters import HtmlFormatter
@@ -36,6 +37,7 @@ from airflow.utils import timezone
 from airflow.utils.code_utils import get_python_source
 from airflow.utils.json import AirflowJsonEncoder
 from airflow.utils.state import State
+from airflow.www.widgets import AirflowDateTimePickerWidget
 
 DEFAULT_SENSITIVE_VARIABLE_FIELDS = (
     'password',
@@ -256,10 +258,14 @@ def nobr_f(attr_name):
 def datetime_f(attr_name):
     def dt(attr):
         f = attr.get(attr_name)
-        f = f.isoformat() if f else ''
+        as_iso = f.isoformat() if f else ''
+        if not as_iso:
+            return Markup('')
+        f = as_iso
         if timezone.utcnow().isoformat()[:4] == f[:4]:
             f = f[5:]
-        return Markup("<nobr>{}</nobr>").format(f)
+        # The empty title will be replaced in JS code when non-UTC dates are displayed
+        return Markup('<nobr><time title="" datetime="{}">{}</time></nobr>').format(as_iso, f)
     return dt
 
 
@@ -424,6 +430,15 @@ class CustomSQLAInterface(SQLAInterface):
         return False
 
     filter_converter_class = UtcAwareFilterConverter
+
+
+# This class is used directly (i.e. we cant tell Fab to use a different
+# subclass) so we have no other option than to edit the converstion table in
+# place
+FieldConverter.conversion_table = (
+    (('is_utcdatetime', DateTimeField, AirflowDateTimePickerWidget),) +
+    FieldConverter.conversion_table
+)
 
 
 def get_dag(orm_dag: DagModel, store_serialized_dags=False):

tests/www/test_security.py
@@ -22,13 +22,13 @@ from unittest import mock
 
 from flask import Flask
 from flask_appbuilder import SQLA, AppBuilder, Model, expose, has_access
-from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder.security.sqla import models as sqla_models
 from flask_appbuilder.views import BaseView, ModelView
 from sqlalchemy import Column, Date, Float, Integer, String
 
 from airflow.exceptions import AirflowException
 from airflow.www.security import AirflowSecurityManager
+from airflow.www.utils import CustomSQLAInterface
 from tests.test_utils.mock_security_manager import MockSecurityManager
 
 READ_WRITE = {'can_dag_read', 'can_dag_edit'}
@@ -51,7 +51,7 @@ class SomeModel(Model):
 
 
 class SomeModelView(ModelView):
-    datamodel = SQLAInterface(SomeModel)
+    datamodel = CustomSQLAInterface(SomeModel)
     base_permissions = ['can_list', 'can_show', 'can_add', 'can_edit', 'can_delete']
     list_columns = ['field_string', 'field_integer', 'field_float', 'field_date']
 

GoogleCloudPlatform/composer-airflow

(cherry picked from commit cc9b1bc78df1e82da37b885d2a8aaf5779c6209d)
GitOrigin-RevId: f8f0bec38be5c0cf34ee80243f14d03ea15522fe

airflow/www_rbac/security.py
@@ -25,9 +25,10 @@ from sqlalchemy import or_, and_
 
 from airflow import models
 from airflow.exceptions import AirflowException
-from airflow.www_rbac.app import appbuilder
 from airflow.utils.db import provide_session
 from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.www_rbac.app import appbuilder
+from airflow.www_rbac.utils import CustomSQLAInterface
 
 
 EXISTING_ROLES = {
@@ -171,6 +172,20 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             'vms': VIEWER_VMS | DAG_VMS | USER_VMS | OP_VMS,
         },
     ]
+
+    def __init__(self, appbuilder):
+        super(AirflowSecurityManager, self).__init__(appbuilder)
+
+        # Go and fix up the SQLAInterface used from the stock one to our subclass.
+        # This is needed to support the "hack" where we had to edit
+        # FieldConverter.conversion_table in place in airflow.www.utils
+        for attr in dir(self):
+            if not attr.endswith('view'):
+                continue
+            view = getattr(self, attr, None)
+            if not view or not getattr(view, 'datamodel', None):
+                continue
+            view.datamodel = CustomSQLAInterface(view.datamodel.obj)
 
     def init_role(self, role_name, role_vms, role_perms):
         """

airflow/www_rbac/utils.py
@@ -32,6 +32,7 @@ from past.builtins import basestring
 from pygments import highlight, lexers
 from pygments.formatters import HtmlFormatter
 from flask import request, Response, Markup, url_for
+from flask_appbuilder.forms import DateTimeField, FieldConverter
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 import flask_appbuilder.models.sqla.filters as fab_sqlafilters
 import sqlalchemy as sqla
@@ -43,6 +44,7 @@ from airflow.operators.subdag_operator import SubDagOperator
 from airflow.utils import timezone
 from airflow.utils.json import AirflowJsonEncoder
 from airflow.utils.state import State
+from airflow.www_rbac.widgets import AirflowDateTimePickerWidget
 
 AUTHENTICATE = conf.getboolean('webserver', 'AUTHENTICATE')
 
@@ -270,10 +272,14 @@ def nobr_f(attr_name):
 def datetime_f(attr_name):
     def dt(attr):
         f = attr.get(attr_name)
-        f = f.isoformat() if f else ''
+        as_iso = f.isoformat() if f else ''
+        if not as_iso:
+            return Markup('')
+        f = as_iso
         if timezone.utcnow().isoformat()[:4] == f[:4]:
             f = f[5:]
-        return Markup("<nobr>{}</nobr>").format(f)
+        # The empty title will be replaced in JS code when non-UTC dates are displayed
+        return Markup('<nobr><time title="" datetime="{}">{}</time></nobr>').format(as_iso, f)
     return dt
 
 
@@ -468,3 +474,12 @@ class CustomSQLAInterface(SQLAInterface):
         return False
 
     filter_converter_class = UtcAwareFilterConverter
+
+
+# This class is used directly (i.e. we cant tell Fab to use a different
+# subclass) so we have no other option than to edit the converstion table in
+# place
+FieldConverter.conversion_table = (
+    (('is_utcdatetime', DateTimeField, AirflowDateTimePickerWidget),) +
+    FieldConverter.conversion_table
+)

tests/www_rbac/test_security.py
@@ -24,16 +24,15 @@ import logging
 import mock
 
 from flask import Flask
-from flask_appbuilder import AppBuilder, SQLA, Model, has_access, expose
-from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import SQLA, AppBuilder, Model, expose, has_access
 from flask_appbuilder.security.sqla import models as sqla_models
 from flask_appbuilder.views import ModelView, BaseView
 from sqlalchemy import Column, Integer, String, Float, Date
 
-from tests.test_utils.mock_security_manager import MockSecurityManager
-
 from airflow.exceptions import AirflowException
 from airflow.www_rbac.security import AirflowSecurityManager
+from airflow.www_rbac.utils import CustomSQLAInterface
+from tests.test_utils.mock_security_manager import MockSecurityManager
 
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
@@ -53,7 +52,7 @@ class SomeModel(Model):
 
 
 class SomeModelView(ModelView):
-    datamodel = SQLAInterface(SomeModel)
+    datamodel = CustomSQLAInterface(SomeModel)
     base_permissions = ['can_list', 'can_show', 'can_add', 'can_edit', 'can_delete']
     list_columns = ['field_string', 'field_integer', 'field_float', 'field_date']
 