6f422d2a6e0a9520a2ae9eb81484b3f7533b91a3
Cloud Composer Team <no-reply@google.com>
2022-01-06 16:12:30 +0200

    squash! Composer core patch
    Changes to add to original commit:
    - fail tasks that are using deferrable operators (temporary until Composer will support Airflow triggerer)
    
    Change-Id: Ida12f239135e0d0f66b24f40e5c3e0c9be12ae4f
    GitOrigin-RevId: 32320b0911e404c97485b05bbf2adc36c21d906d

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -1655,6 +1655,15 @@ class BaseOperator(Operator, LoggingMixin, TaskMixin, metaclass=BaseOperatorMeta
         This is achieved by raising a special exception (TaskDeferred)
         which is caught in the main _execute_task wrapper.
         """
+        from airflow.composer.utils import get_composer_version
+
+        if get_composer_version() is not None:
+            # This if statement will allow to bypass community tests.
+            raise AirflowException(
+                "Composer doesn't support deferrable operators yet. Tasks that are using "
+                "deferrable operators will fail to execute with this message."
+            )
+
         raise TaskDeferred(trigger=trigger, method_name=method_name, kwargs=kwargs, timeout=timeout)
 
 
