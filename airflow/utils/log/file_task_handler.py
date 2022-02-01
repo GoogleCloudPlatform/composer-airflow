d519cef1f9047069a8c417c207e836c59502189b
Cloud Composer Team <no-reply@google.com>
2022-02-01 14:54:09 +0000

    fixup! Composer logging/metrics patch
    Add try number in log labels
    
    Change-Id: I53f072d4a79a6074369fb3468a6ed0ce1a3de131
    GitOrigin-RevId: 82171ed1476e0bd9aa27d8d8a3af2b831263b5f2

--- airflow/utils/log/file_task_handler.py
+++ airflow/utils/log/file_task_handler.py
@@ -76,6 +76,7 @@ class StreamTaskHandler(logging.StreamHandler):
             'workflow': ti.dag_id,
             'task-id': ti.task_id,
             'execution-date': ti.execution_date.isoformat(),
+            'try-number': ti.try_number,
         }
 
     def emit(self, record):
@@ -115,6 +116,7 @@ class FileTaskHandler(logging.Handler):
             'workflow': ti.dag_id,
             'task-id': ti.task_id,
             'execution-date': ti.execution_date.isoformat(),
+            'try-number': ti.try_number,
         }
 
     def emit(self, record):
