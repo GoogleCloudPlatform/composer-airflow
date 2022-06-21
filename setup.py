b7de1c57020aec734477f0fffc7de7c40ffb38fe
Cloud Composer Team <no-reply@google.com>
2022-06-21 10:24:07 +0000

    fixup! Composer dependencies patch
    Update google provider package to 2022.6.14+composer
    
    Change-Id: Ib35a699a28c20db411a76a0d5f6719722556a3c2
    GitOrigin-RevId: 0f6f76c9191258f33c6b8005e5091f4fca007de1

--- setup.py
+++ setup.py
@@ -1085,7 +1085,7 @@ def add_all_provider_packages() -> None:
             # TODO: (should be removed in Airflow 2.3.0+ and in current Airflow version once we decide to
             # release 7.0.0+ package in Composer) this is our internal release because customers are
             # not ready to migrate to google provider package 7.0.0+
-            "google": "==2022.5.18+composer",
+            "google": "==2022.6.14+composer",
             # postgres provider 3.0.0+ the URIs returned by Postgres get_uri() returns postgresql://
             # instead of postgres:// prefix which is the only supported prefix for the SQLAlchemy 1.4.0+.
             # restriction needed because some tests failing.
