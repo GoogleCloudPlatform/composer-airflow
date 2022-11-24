3383a87fc9644e958e7efa98feec89aab39559e6
Maksim Moiseenkov <moiseenkov@google.com>
2022-11-24 13:45:45 +0000

    fixup! Composer data lineage patch
    Changes(b/259210404):
    - support wildcard in GCSToGCSOperatorLineageMixin
    - update unit tests
    
    Change-Id: I38c8e71d2f98325f02135157f674318132a41bbb
    GitOrigin-RevId: 394bbba1ac74a0ee398ce64af25762c538bcf273

--- airflow/composer/data_lineage/transfers/google/cloud/gcs_to_gcs.py
+++ airflow/composer/data_lineage/transfers/google/cloud/gcs_to_gcs.py
@@ -16,7 +16,6 @@ import logging
 from typing import Dict
 
 from airflow.composer.data_lineage.entities import GCSEntity
-from airflow.providers.google.cloud.transfers.gcs_to_gcs import WILDCARD
 
 log = logging.getLogger(__name__)
 
@@ -25,24 +24,14 @@ class GCSToGCSOperatorLineageMixin:
     """Mixin class for GCSToGCSOperator."""
 
     def post_execute_prepare_lineage(self, context: Dict):
-        _source_paths = [self.source_object] if self.source_object else self.source_objects
-        inlet_paths = []
-        for _source_path in _source_paths:
-            if WILDCARD in _source_path:
-                # We report object's folder if wildcard was used, for example:
-                # 'sales/sales-2017/prefix_*.avro' => 'sales/sales-2017/'
-                _source_path = _source_path.split(WILDCARD)[0]
-                _source_path = _source_path.rsplit('/', 1)[0] + '/'
-            inlet_paths.append(_source_path)
-        inlets = [GCSEntity(bucket=self.source_bucket, path=path) for path in inlet_paths]
+        source_paths = [self.source_object] if self.source_object else self.source_objects
+        inlets = [GCSEntity(bucket=self.source_bucket, path=path) for path in source_paths]
 
         destination_bucket = self.destination_bucket or self.source_bucket
         if self.destination_object:
-            # TODO: we should here cover cases, described in the bug b/258635213 immediately after getting
-            #  it fixed or GCSToGCSOperator refactored
             outlets = [GCSEntity(bucket=destination_bucket, path=self.destination_object)]
         else:
-            outlets = [GCSEntity(bucket=destination_bucket, path=path) for path in inlet_paths]
+            outlets = [GCSEntity(bucket=destination_bucket, path=path) for path in source_paths]
 
         self.inlets.extend(inlets)
         self.outlets.extend(outlets)
