9ec2f621383d67b2f25f05d7a91f43d031ec7cbd
Cloud Composer Team <no-reply@google.com>
2024-09-03 10:35:29 +0200

    fixup! Composer kubernetes patch
    Fix issue with recursion exceeded in KubernetesPodOperator for Composer 3.
    Internal bug
    
    PUBLIC_RELNOTES_BEGIN
    Composer 3. Fix failing of KubernetesPodOperator running for more than 15 mins.
    PUBLIC_RELNOTES_END
    
    Change-Id: I2e14ab927d037b5703ba3de581282211a75d10ea
    GitOrigin-RevId: 659dd31126f4dac3cb677106fe8f169c2be1f5e7

--- airflow/composer/kubernetes/pod_manager.py
+++ airflow/composer/kubernetes/pod_manager.py
@@ -94,39 +94,32 @@ def _stream_peer_vm_logs(self, pod, container_name, peer_vm_endpoint, after_time
          peer_vm_endpoint: endpoint of the Peer VM, to retrieve logs.
          after_timestamp: timestamp since query logs in RFC 3339 format.
     """
-    is_last_iteration = not self.container_is_running(pod, container_name=PEER_VM_PLACEHOLDER_CONTAINER)
-    time.sleep(SLEEP_BETWEEN_PEER_VM_LOGS_STREAMING_ITERATIONS)
+    while True:
+        is_last_iteration = not self.container_is_running(pod, container_name=PEER_VM_PLACEHOLDER_CONTAINER)
+        time.sleep(SLEEP_BETWEEN_PEER_VM_LOGS_STREAMING_ITERATIONS)
 
-    url = f"http://{peer_vm_endpoint}:9080/logs"
-    params = {
-        "container_name": container_name,
-        "after_timestamp": after_timestamp,
-        "max_log_lines": 1000,
-    }
-    self.log.debug("Reading logs, url: %s, params: %s", url, params)
-    try:
-        response = requests.get(url, params=params)
-    except Exception as e:
-        self.log.debug("Exception occurred on request: %s", e)
-    else:
-        if response.status_code != 200:
-            self.log.debug("Got %s response, reason: %s", response.status_code, response.reason)
+        url = f"http://{peer_vm_endpoint}:9080/logs"
+        params = {
+            "container_name": container_name,
+            "after_timestamp": after_timestamp,
+            "max_log_lines": 1000,
+        }
+        self.log.debug("Reading logs, url: %s, params: %s", url, params)
+        try:
+            response = requests.get(url, params=params)
+        except Exception as e:
+            self.log.debug("Exception occurred on request: %s", e)
         else:
-            for log in response.json()["logs"] or []:
-                # Example of log: "2023-05-02T10:11:12.2Z stdout F Creating dataset"
-                after_timestamp, _, _, msg = log.split(" ", 3)
-                self.log.info(msg)
+            if response.status_code != 200:
+                self.log.debug("Got %s response, reason: %s", response.status_code, response.reason)
+            else:
+                for log in response.json()["logs"] or []:
+                    # Example of log: "2023-05-02T10:11:12.2Z stdout F Creating dataset"
+                    after_timestamp, _, _, msg = log.split(" ", 3)
+                    self.log.info(msg)
 
-    if is_last_iteration:
-        return
-
-    _stream_peer_vm_logs(
-        self,
-        pod=pod,
-        container_name=container_name,
-        peer_vm_endpoint=peer_vm_endpoint,
-        after_timestamp=after_timestamp,
-    )
+        if is_last_iteration:
+            break
 
 
 def _composer_get_container_names(f):
