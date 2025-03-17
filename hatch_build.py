b70c46d6001a5fd9f29bd7f6e65822d790e27f7f
Cloud Composer Team <no-reply@google.com>
2025-03-17 14:47:44 +0000

    fixup! Composer dependencies patch
    Pin cncf package to <= 10.1.0 until b/403570146 is fixed.
    
    Change-Id: I92f5c9ced319e5836836310a62d53f5741fd5261
    GitOrigin-RevId: 6d611b171442d50e108ec6cac6cd589652147aab

--- hatch_build.py
+++ hatch_build.py
@@ -504,7 +504,8 @@ COMPOSER_DEPENDENCIES = [
     # Celery provider 3.9.0 breaks for airflow < 3, will be fixed in 3.9.1
     # TODO: Internal bug - Also Celery provider >3.10.0 breaks for Airflow <= 2.10.5
     "apache-airflow-providers-celery <=3.10.0,!=3.9.0",
-    "apache-airflow-providers-cncf-kubernetes",
+    # TODO: Internal bug - Remove the constraint once the bug is fixed in composer-airflow
+    "apache-airflow-providers-cncf-kubernetes<=10.1.0",
     "apache-airflow-providers-dbt-cloud",
     "apache-airflow-providers-google",
     "apache-airflow-providers-hashicorp",
