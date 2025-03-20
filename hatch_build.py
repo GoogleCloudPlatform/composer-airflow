e166f587ff0019f3c7a18ee7fa7c740101344baa
Cloud Composer Team <no-reply@google.com>
2025-03-20 15:55:59 +0000

    fixup! Composer dependencies patch
    Remove celery providers constraint as the guilty version was yanked.
    
    Change-Id: Iac6842a49919d4e2c4b8ad29353211dbc450de8b
    (cherry picked from commit c5190103ed169cd696ee5cb1b66655cba67d0eb2)
    GitOrigin-RevId: fd086597d35f67ddf520aa2323ad3e6c568feea0

--- hatch_build.py
+++ hatch_build.py
@@ -508,8 +508,7 @@ DEPENDENCIES = [
 COMPOSER_DEPENDENCIES = [
     "apache-airflow-providers-apache-beam",
     # Celery provider 3.9.0 breaks for airflow < 3, will be fixed in 3.9.1
-    # TODO: Internal bug - Also Celery provider >3.10.0 breaks for Airflow <= 2.10.5
-    "apache-airflow-providers-celery <=3.10.0,!=3.9.0",
+    "apache-airflow-providers-celery!=3.9.0",
     # TODO: Internal bug - Remove the constraint once the bug is fixed in composer-airflow
     "apache-airflow-providers-cncf-kubernetes<=10.1.0",
     "apache-airflow-providers-dbt-cloud",
