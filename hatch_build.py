b645b60c59dd628eba70ba847fcb953506e2c52d
Cloud Composer Team <no-reply@google.com>
2025-05-29 14:34:07 +0000

    Composer dependencies patch
    Changes:
    - change version comparison operator to avoid only version 10.4.3 of kubernetes provider.
    
    Change-Id: I604367a0c82d6b962287e48f0457b5ac79b3d1aa
    GitOrigin-RevId: f46b37c612fef17632beb570c128e909b6d95c3d

--- hatch_build.py
+++ hatch_build.py
@@ -511,8 +511,9 @@ COMPOSER_DEPENDENCIES = [
     # Celery provider 3.9.0 breaks for airflow < 3, will be fixed in 3.9.1
     "apache-airflow-providers-celery!=3.9.0",
     # In version 10.4.3 of the kubernetes provider community introduced AF 2.9.3 incompatible import.
-    # https://github.com/apache/airflow/pull/49186
-    "apache-airflow-providers-cncf-kubernetes<10.4.3",
+    # https://github.com/apache/airflow/pull/49186 But revert the changes in 10.5.0
+    # https://github.com/apache/airflow/pull/50651
+    "apache-airflow-providers-cncf-kubernetes!=10.4.3",
     "apache-airflow-providers-dbt-cloud",
     # fab provider>=1.3.0 requires changes in Airflow core https://github.com/apache/airflow/pull/40703
     # TODO: remove this constraint in Airflow 2.10.0+.
