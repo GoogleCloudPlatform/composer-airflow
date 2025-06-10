1ca6ead157912dce1e97c5c533503486b609a1dd
Cloud Composer Team <no-reply@google.com>
2025-06-10 12:40:35 +0000

    Composer lineage patch
    Changes:
    - add ComposerTransport for OpenLineage support
    - add Composer custom extractor for DataprocSubmitJobOperator
    
    Change-Id: I55a2a26f5a64c6ff5d05e41ce410f3d02372799e
    GitOrigin-RevId: 240228de4dcc0c0b942a275faf844abfbccd459a

---
+++ airflow-core/src/airflow/composer/patches/lineage/openlineage/composer_transport.py
@@ -0,0 +1,158 @@
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import json
+import os
+from typing import TYPE_CHECKING
+
+import structlog
+from google.api_core.retry import Retry
+from google.cloud.datacatalog.lineage.producer_client.v1 import SyncLineageClient
+from google.cloud.datacatalog_lineage_v1 import ProcessOpenLineageRunEventRequest
+from openlineage.client.serde import Serde
+from openlineage.client.transport.transport import Config, Transport
+
+from airflow.composer.patches.lineage.openlineage.facets import (
+    ComposerJobFacet,
+    ComposerRunFacet,
+    GcpLineageJobFacet,
+    GcpOrigin,
+)
+from airflow.composer.patches.lineage.openlineage.utils import get_redacted_event, sanitize_display_name
+from airflow.version import version as airflow_version
+
+if TYPE_CHECKING:
+    from openlineage.client.client import Event
+
+# Side channel header according to Cloud Data Lineage documentation.
+SIDECHANNEL_HEADER = "x-goog-ext-512598505-bin"
+SIDECHANNEL_VALUES = {
+    "DAG": b"\n\x08COMPOSER\x12\x03DAG",
+    "TASK": b"\n\x08COMPOSER\x12\x04TASK",
+}
+UNKNOWN_TYPE_SIDECHANNEL_VALUE = b"\n\x08COMPOSER\x12\x07UNKNOWN"
+
+COMPOSER_ENVIRONMENT_NAME = os.environ.get("COMPOSER_ENVIRONMENT")
+LOCATION_PATH = f"projects/{os.environ.get('GCP_PROJECT')}/locations/{os.environ.get('COMPOSER_LOCATION')}"
+COMPOSER_VERSION = os.environ.get("COMPOSER_VERSION")
+
+log = structlog.get_logger(logger_name=__name__, composer_extra_info={"log-type": "data_lineage"})
+
+
+class ComposerTransportConfig(Config):
+    """Config Class for Composer Transport."""
+
+    ...
+
+
+class ComposerTransport(Transport):
+    """
+    Transport layer that sends events to the Cloud Data Lineage OL Endpoint.
+
+    Also injects Composer and GCP specific facets to each event.
+    """
+
+    kind = "ComposerTransport"
+    config_class = ComposerTransportConfig
+
+    def __init__(self, config: ComposerTransportConfig) -> None:
+        self.client = SyncLineageClient()
+
+    def emit(self, event: Event) -> None:
+        try:
+            log.info("Sending the event to the Data Lineage OpenLineage API.")
+            self._patch_event(event)
+            event_dict = json.loads(Serde.to_json(event))
+            log.info(str(get_redacted_event(event_dict)))
+
+            # We use the openlineage SerDe module to send the event as a dictionary
+            request = ProcessOpenLineageRunEventRequest({"parent": LOCATION_PATH, "open_lineage": event_dict})
+            job_type = event.job.facets["jobType"].jobType
+            sidechannel_value = SIDECHANNEL_VALUES.get(job_type, UNKNOWN_TYPE_SIDECHANNEL_VALUE)
+            response = self.client.process_open_lineage_run_event(
+                request,
+                metadata=[
+                    (SIDECHANNEL_HEADER, sidechannel_value),
+                ],
+                retry=Retry(deadline=5),
+            )
+            response_message = f"response: process={response.process}, run={response.run}"
+            if response.lineage_events:
+                response_message = "\n".join([response_message, f"lineage_events={response.lineage_events}"])
+            log.info(response_message)
+        except Exception:
+            log.exception("Failed to send the event to DataLineage API.")
+
+    def _patch_event(self, event: Event) -> None:
+        """Add Composer and GCP specific facets to the event."""
+        job_type = event.job.facets["jobType"].jobType
+        if job_type == "DAG":
+            composer_job_facet = ComposerJobFacet(
+                environmentName=COMPOSER_ENVIRONMENT_NAME,
+                composerVersion=COMPOSER_VERSION,
+                airflowVersion=airflow_version,
+                dagId=event.job.name,
+                taskId=None,
+                operator=None,
+            )
+            run_id = (
+                event.run.facets["airflowDagRun"].dagRun["run_id"]
+                if event.run.facets.get("airflowDagRun")
+                else None
+            )
+            composer_run_facet = ComposerRunFacet(dagRunId=run_id)
+        elif job_type == "TASK":
+            composer_job_facet = ComposerJobFacet(
+                environmentName=COMPOSER_ENVIRONMENT_NAME,
+                composerVersion=COMPOSER_VERSION,
+                airflowVersion=airflow_version,
+                dagId=event.run.facets["airflow"].dag["dag_id"],
+                taskId=event.run.facets["airflow"].task["task_id"],
+                operator=event.run.facets["airflow"].task["operator_class"],
+            )
+            composer_run_facet = ComposerRunFacet(dagRunId=event.run.facets["airflow"].dagRun["run_id"])
+        else:
+            log.warning("Unrecognized OpenLineage JobType %s", job_type)
+            return
+
+        gcp_lineage_facet = GcpLineageJobFacet(
+            displayName=sanitize_display_name(
+                f"Composer Airflow {job_type.capitalize()} {COMPOSER_ENVIRONMENT_NAME}.{event.job.name}"
+            ),
+            origin=GcpOrigin(
+                sourceType="COMPOSER",
+                name=os.path.join(LOCATION_PATH, f"environments/{COMPOSER_ENVIRONMENT_NAME}"),
+            ),
+        )
+
+        event.job.facets["gcp_composer_job"] = composer_job_facet
+        event.run.facets["gcp_composer_run"] = composer_run_facet
+        event.job.facets["gcp_lineage"] = gcp_lineage_facet
+
+        self._patch_bigquery_external_query_if_needed(event)
+
+    def _patch_bigquery_external_query_if_needed(self, event: Event) -> None:
+        if event.run.facets.get("externalQuery") and event.run.facets["externalQuery"].source == "bigquery":
+            if event.run.facets.get("bigQueryJob") is None:
+                log.debug(
+                    "run.facets.bigQueryJob facet not found when patching the BigQuery externalQuery facet"
+                )
+                return
+            try:
+                bigqueryJobId = json.loads(event.run.facets["bigQueryJob"].properties)["id"]
+                event.run.facets["externalQuery"].externalQueryId = bigqueryJobId
+            except json.JSONDecodeError:
+                log.warning("Failed to parse BigQuery JobId")
---
+++ airflow-core/tests/unit/composer/patches/lineage/openlineage/extractors/test_dataproc_extractor.py
@@ -0,0 +1,141 @@
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+from unittest import mock
+
+from google.api_core.exceptions import NotFound
+
+from airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor import DataprocExtractor
+from airflow.providers.common.compat.openlineage.facet import Dataset
+from airflow.providers.google.cloud.operators.dataproc import DataprocSubmitJobOperator
+from airflow.providers.openlineage.extractors.base import OperatorLineage
+
+INPUTS = [Dataset(namespace="dataproc_metastore", name="inputtable")]
+OUTPUTS = [Dataset(namespace="dataproc_metastore", name="outputtable")]
+TEST_REGION = "test_region"
+TEST_TASK_ID = "test_task_id"
+TEST_PROJECT_ID = "test_project_id"
+TEST_GCP_CONN_ID = "test_gcp_conn_id"
+TEST_IMPERSONATION_CHAIN = "test_impersonation_chain"
+
+
+class TestDataprocExtractor:
+    def test_extractor_linking(self):
+        assert DataprocExtractor.get_operator_classnames() == ["DataprocSubmitJobOperator"]
+
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocSQLJobLineageExtractor",
+        autospec=True,
+    )
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocHook",
+        autospec=True,
+    )
+    @mock.patch("airflow.composer.patches.lineage.openlineage.extractors.utils.xcom_pull", autospec=True)
+    def test_extract_on_complete(self, mock_xcom_pull, mock_dataproc_hook, mock_dataproc_sql_extractor):
+        operator = DataprocSubmitJobOperator(
+            task_id=TEST_TASK_ID,
+            job=mock.MagicMock(),
+            region=TEST_REGION,
+            project_id=TEST_PROJECT_ID,
+            gcp_conn_id=TEST_GCP_CONN_ID,
+            impersonation_chain=TEST_IMPERSONATION_CHAIN,
+        )
+        mock_dataproc_sql_extractor.return_value.data_lineage.return_value = (INPUTS, OUTPUTS)
+        task_instance = mock.MagicMock()
+
+        metadata_on_complete = DataprocExtractor(operator).extract_on_complete(task_instance=task_instance)
+
+        expected_task_metadata = OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+        )
+
+        assert metadata_on_complete == expected_task_metadata
+
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocSQLJobLineageExtractor",
+        autospec=True,
+    )
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocHook",
+        autospec=True,
+    )
+    @mock.patch("airflow.composer.patches.lineage.openlineage.extractors.utils.xcom_pull", autospec=True)
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.log", autospec=True
+    )
+    def test_extract_on_complete_no_project_id(
+        self, mock_log, mock_xcom_pull, mock_dataproc_hook, mock_dataproc_sql_extractor
+    ):
+        operator = DataprocSubmitJobOperator(
+            task_id=TEST_TASK_ID,
+            job=mock.MagicMock(),
+            region=TEST_REGION,
+            project_id=TEST_PROJECT_ID,
+            gcp_conn_id=TEST_GCP_CONN_ID,
+            impersonation_chain=TEST_IMPERSONATION_CHAIN,
+        )
+        mock_dataproc_sql_extractor.return_value.data_lineage.return_value = (INPUTS, OUTPUTS)
+        task_instance = mock.MagicMock()
+        mock_dataproc_hook_inst = mock_dataproc_hook.return_value
+        mock_dataproc_hook_inst.project_id = None
+
+        metadata_on_complete = DataprocExtractor(operator).extract_on_complete(task_instance=task_instance)
+
+        expected_task_metadata = OperatorLineage()
+
+        assert metadata_on_complete == expected_task_metadata
+        mock_log.exception.assert_called_once_with("The project_id is missing. Data lineage wasn't reported")
+
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocSQLJobLineageExtractor",
+        autospec=True,
+    )
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.DataprocHook",
+        autospec=True,
+    )
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.xcom_pull", autospec=True
+    )
+    @mock.patch(
+        "airflow.composer.patches.lineage.openlineage.extractors.dataproc_extractor.log", autospec=True
+    )
+    def test_extract_on_complete_job_id_not_found(
+        self, mock_log, mock_xcom_pull, mock_dataproc_hook, mock_dataproc_sql_extractor
+    ):
+        operator = DataprocSubmitJobOperator(
+            task_id=TEST_TASK_ID,
+            job=mock.MagicMock(),
+            region=TEST_REGION,
+            project_id=TEST_PROJECT_ID,
+            gcp_conn_id=TEST_GCP_CONN_ID,
+            impersonation_chain=TEST_IMPERSONATION_CHAIN,
+        )
+        mock_dataproc_sql_extractor.return_value.data_lineage.return_value = (INPUTS, OUTPUTS)
+        task_instance = mock.MagicMock()
+        mock_xcom_pull.return_value = "job-id"
+        mock_dataproc_hook.return_value.get_job.side_effect = NotFound("Job not found.")
+
+        metadata_on_complete = DataprocExtractor(operator).extract_on_complete(task_instance=task_instance)
+
+        expected_task_metadata = OperatorLineage()
+
+        assert metadata_on_complete == expected_task_metadata
+        mock_log.exception.assert_called_once_with(
+            "The job with id %s wasn't found. Data lineage wasn't reported", "job-id"
+        )
