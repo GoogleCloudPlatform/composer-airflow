8ff476d3e8085fb44e8e03fe06dad8ba0e9e8e54
Cloud Composer Team <no-reply@google.com>
2025-06-13 11:54:03 +0200

    Composer airflow-core patch
    Changes:
    - fix link to Airflow documentation in "Docs" tab
    
    Change-Id: Ib347877c111be8669a59493e97865b7a936eb7aa
    GitOrigin-RevId: 868ae971a2659688b6f488edacf3f740b749e221

--- airflow-core/src/airflow/ui/src/layouts/Nav/Nav.tsx
+++ airflow-core/src/airflow/ui/src/layouts/Nav/Nav.tsx
@@ -179,7 +179,7 @@ export const Nav = () => {
         <DocsButton
           externalViews={docsItems}
           showAPI={authLinks?.authorized_menu_items.includes("Docs")}
-          version={data?.version}
+          version={data?.version.replace("+composer", "")}
         />
         <UserSettingsButton externalViews={userItems} />
       </Flex>
