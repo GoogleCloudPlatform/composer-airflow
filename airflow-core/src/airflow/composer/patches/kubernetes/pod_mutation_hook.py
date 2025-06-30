f3fac708108bd2341da621e2cc038343846794ff
Cloud Composer Team <no-reply@google.com>
2025-06-30 11:27:21 +0200

    Composer kubernetes patch
    Changes:
    - implement pod_mutation_hook for KubernetesPodOperator and KubernetesExecutor support
    - patch PodManager to support running KubernetesPodOperator
    
    Change-Id: I0c7a33b65cd99d4456f2b3f1d496d1051241603b
    GitOrigin-RevId: da3cfd7bb12210fefe6d4afdf60e6b2772047783

---
+++ airflow-core/src/airflow/composer/patches/kubernetes/pod_mutation_hook.py
@@ -0,0 +1,181 @@
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+import logging
+import math
+import os
+
+import yaml
+from kubernetes.client import Configuration, models as k8s
+from kubernetes.utils import parse_quantity
+
+from airflow.composer.patches.core.utils import get_composer_gke_cluster_host, is_currently_running_component
+from airflow.providers.cncf.kubernetes.pod_generator import PodGenerator
+
+MAX_RESOURCES_DISK_SIZE_GB = 100
+USER_WORKLOADS_NAMESPACE = "composer-user-workloads"
+USE_WORKLOAD_IDENTITY_SERVICE_LABEL = "node.gke.io/use-workload-identity-service"
+GCE_VM_ANNOTATION = "node.gke.io/gce-vm"
+EPS = 1e-9
+
+logger = logging.getLogger(__name__)
+
+
+def mutate(pod: k8s.V1Pod):
+    # In case of running pod by KPO or KubernetesExecutor we should adjust pod spec.
+    # Note, that we check below if cluster host where pod will be deployed is a Composer GKE cluster host, to
+    # account for GKEStartPodOperator which runs pods in external GKE cluster.
+    # In case of scheduler (KubernetesExecutor), the logic with checking host doesn't work, but we can check
+    # if it is scheduler by inspecting sys.argv.
+    pod_running_in_composer_gke = (
+        is_currently_running_component("scheduler")
+        or Configuration.get_default_copy().host == get_composer_gke_cluster_host()
+    )
+    if not pod_running_in_composer_gke:
+        return
+
+    logger.info("Modifying pod spec")
+    pod.metadata = PodGenerator.reconcile_metadata(pod.metadata, _get_peer_vm_pod_metadata(pod))
+
+    # Clear resources, they are provided via pod.metadata.
+    pod.spec.containers[0].resources = None
+
+
+def _get_peer_vm_pod_metadata(pod: k8s.V1Pod):
+    """Return metadata for container-based tasks."""
+    tenant_project_id = os.environ["GCP_TENANT_PROJECT"]
+
+    resources_disk_size_gb = 0
+    if pod.spec.containers[0].resources:
+        requests_gb = 0
+        limits_gb = 0
+        if pod.spec.containers[0].resources.requests:
+            requests_gb = math.ceil(
+                parse_quantity(pod.spec.containers[0].resources.requests.get("ephemeral-storage", "0"))
+                / 1000
+                / 1000
+                / 1000
+            )  # round up to integer number of GBs.
+        if pod.spec.containers[0].resources.limits:
+            limits_gb = math.ceil(
+                parse_quantity(pod.spec.containers[0].resources.limits.get("ephemeral-storage", "0"))
+                / 1000
+                / 1000
+                / 1000
+            )  # round up to integer number of GBs.
+        resources_disk_size_gb = max(requests_gb, limits_gb)
+    if resources_disk_size_gb > MAX_RESOURCES_DISK_SIZE_GB:
+        logger.warning(
+            "Resources disk size is %sGB which is greater than maximum allowed %sGB",
+            resources_disk_size_gb,
+            MAX_RESOURCES_DISK_SIZE_GB,
+        )
+        resources_disk_size_gb = MAX_RESOURCES_DISK_SIZE_GB
+    disk_size_gb = 30 + resources_disk_size_gb  # add 30 GB for operating system, container images etc.
+
+    return k8s.V1ObjectMeta(
+        # All container-based tasks should be running in a dedicated "user workloads namespace".
+        namespace=USER_WORKLOADS_NAMESPACE,
+        labels={
+            USE_WORKLOAD_IDENTITY_SERVICE_LABEL: "true",
+        },
+        annotations={
+            GCE_VM_ANNOTATION: yaml.dump(
+                {
+                    "selector": {
+                        "matchLabels": {
+                            "machineType": _get_peer_vm_machine_type(pod.spec.containers[0].resources),
+                            "diskSizeGb": str(disk_size_gb),
+                        }
+                    },
+                    "logging": ["Workload", "System"],
+                    "vmServiceAccount": f"peervm-vm@{tenant_project_id}.iam.gserviceaccount.com",
+                }
+            ),
+        },
+    )
+
+
+def _get_peer_vm_machine_type(resources: k8s.V1ResourceRequirements) -> str:
+    """Return machine type for container-based tasks."""
+    machine_series = "e2"
+    cpu_amount, cpu_string = _get_peer_vm_machine_cpu(resources)
+    memory_string = _get_peer_vm_machine_memory(resources, cpu_amount)
+
+    return f"{machine_series}-custom-{cpu_string}-{memory_string}"
+
+
+def _get_peer_vm_machine_cpu(resources: k8s.V1ResourceRequirements) -> tuple[float, str]:
+    requests_cpu = None
+    limits_cpu = None
+    if resources and resources.requests and resources.requests.get("cpu"):
+        requests_cpu = resources.requests["cpu"]
+        requests_cpu = float(requests_cpu[:-1]) / 1000 if requests_cpu.endswith("m") else float(requests_cpu)
+    if resources and resources.limits and resources.limits.get("cpu"):
+        limits_cpu = resources.limits["cpu"]
+        limits_cpu = float(limits_cpu[:-1]) / 1000 if limits_cpu.endswith("m") else float(limits_cpu)
+    if (requests_cpu is not None) and (limits_cpu is not None):
+        desired_cpu_amount = max(requests_cpu, limits_cpu)
+    else:
+        desired_cpu_amount = requests_cpu or limits_cpu or 0.5
+
+    # List of valid CPU amount and corresponding presentation.
+    valid_cpu_values = [
+        (0.25, "micro"),
+        (0.5, "small"),
+        (1, "medium"),
+    ] + [(x, str(x)) for x in range(2, 34, 2)]
+    # Find the lowest valid CPU amount that is greater or equal than desired.
+    for valid_cpu_amount, valid_cpu_string in valid_cpu_values:
+        if desired_cpu_amount < valid_cpu_amount + EPS:
+            return valid_cpu_amount, valid_cpu_string
+
+    logger.warning(
+        "Resources CPU is %s which is greater than maximum allowed %s",
+        desired_cpu_amount,
+        valid_cpu_values[-1][1],
+    )
+    return valid_cpu_values[-1]
+
+
+def _get_peer_vm_machine_memory(resources: k8s.V1ResourceRequirements, cpu: float) -> str:
+    requests_memory_gb = None
+    limits_memory_gb = None
+    if resources and resources.requests and resources.requests.get("memory"):
+        requests_memory_gb = parse_quantity(resources.requests["memory"]) / 1000 / 1000 / 1000
+    if resources and resources.limits and resources.limits.get("memory"):
+        limits_memory_gb = parse_quantity(resources.limits["memory"]) / 1000 / 1000 / 1000
+    if (requests_memory_gb is not None) and (limits_memory_gb is not None):
+        desired_memory_gb_amount = max(requests_memory_gb, limits_memory_gb)
+    else:
+        desired_memory_gb_amount = requests_memory_gb or limits_memory_gb or (cpu * 4)
+
+    # List of valid memory amount with corresponding presentation (based on given CPU).
+    # TODO: investigate why KPO/KE tasks are failing with Peer VM having 1GB memory.
+    valid_memory_gb_values = [(x, str(1024 * x)) for x in range(2, 129) if 0.5 - EPS < x / cpu < 8 + EPS]
+    for valid_memory_gb_amount, valid_memory_gb_string in valid_memory_gb_values:
+        if desired_memory_gb_amount < valid_memory_gb_amount + EPS:
+            return valid_memory_gb_string
+
+    logger.warning(
+        "Resources memory is %sGB which is greater than maximum allowed %sGB for the current amount of CPU "
+        "which is %s",
+        desired_memory_gb_amount,
+        valid_memory_gb_values[-1][0],
+        cpu,
+    )
+    return valid_memory_gb_values[-1][1]
---
+++ airflow-core/tests/unit/composer/patches/kubernetes/test_pod_mutation_hook.py
@@ -0,0 +1,382 @@
+#
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import annotations
+
+from unittest import mock
+
+import pytest
+import yaml
+from kubernetes.client import Configuration, models as k8s
+
+from airflow.composer.patches.kubernetes.pod_mutation_hook import (
+    _get_peer_vm_machine_type,
+    _get_peer_vm_pod_metadata,
+    mutate,
+)
+
+
+class TestPodMutationHook:
+    @mock.patch(
+        "airflow.composer.patches.kubernetes.pod_mutation_hook.get_composer_gke_cluster_host",
+        mock.Mock(return_value="http://internal-cluster"),
+    )
+    @mock.patch.dict("os.environ", {"GCP_TENANT_PROJECT": "test-project-234"})
+    def test_mutate_internal_gke_cluster(self):
+        Configuration.set_default(Configuration(host="http://internal-cluster"))
+        pod = k8s.V1Pod(
+            metadata=k8s.V1ObjectMeta(namespace="test"),
+            spec=k8s.V1PodSpec(
+                containers=[
+                    k8s.V1Container(
+                        name="base",
+                        resources=k8s.V1ResourceRequirements(limits={"cpu": "1"}),
+                    )
+                ]
+            ),
+        )
+
+        mutate(pod)
+
+        assert pod.metadata.namespace == "composer-user-workloads"
+        assert pod.spec.containers[0].resources is None
+
+    @mock.patch(
+        "airflow.composer.patches.kubernetes.pod_mutation_hook.get_composer_gke_cluster_host",
+        mock.Mock(return_value="http://internal-cluster"),
+    )
+    def test_mutate_external_gke_cluster(self):
+        Configuration.set_default(Configuration(host="http://external-cluster"))
+        pod = k8s.V1Pod(metadata=k8s.V1ObjectMeta(namespace="test"))
+
+        mutate(pod)
+
+        assert pod.metadata.namespace == "test"
+
+    @mock.patch(
+        "airflow.composer.patches.kubernetes.pod_mutation_hook.get_composer_gke_cluster_host",
+        mock.Mock(return_value="http://internal-cluster"),
+    )
+    @mock.patch.dict("os.environ", {"GCP_TENANT_PROJECT": "test-project-234"})
+    @mock.patch("sys.argv", ["airflow", "scheduler"])
+    def test_mutate_scheduler(self):
+        Configuration.set_default(Configuration(host="http://external-cluster"))
+        pod = k8s.V1Pod(
+            metadata=k8s.V1ObjectMeta(namespace="test"),
+            spec=k8s.V1PodSpec(
+                containers=[
+                    k8s.V1Container(
+                        name="base",
+                        resources=k8s.V1ResourceRequirements(limits={"cpu": "1"}),
+                    )
+                ]
+            ),
+        )
+
+        mutate(pod)
+
+        assert pod.metadata.namespace == "composer-user-workloads"
+        assert pod.spec.containers[0].resources is None
+
+    @mock.patch.dict("os.environ", {"GCP_TENANT_PROJECT": "test-project-234"})
+    def test_get_peer_vm_pod_metadata(self):
+        actual = _get_peer_vm_pod_metadata(
+            pod=k8s.V1Pod(spec=k8s.V1PodSpec(containers=[k8s.V1Container(name="base")]))
+        )
+
+        assert actual == k8s.V1ObjectMeta(
+            namespace="composer-user-workloads",
+            labels={"node.gke.io/use-workload-identity-service": "true"},
+            annotations={
+                "node.gke.io/gce-vm": yaml.dump(
+                    {
+                        "selector": {
+                            "matchLabels": {
+                                "machineType": "e2-custom-small-2048",
+                                "diskSizeGb": "30",
+                            },
+                        },
+                        "logging": ["Workload", "System"],
+                        "vmServiceAccount": "peervm-vm@test-project-234.iam.gserviceaccount.com",
+                    }
+                ),
+            },
+        )
+
+    @pytest.mark.parametrize(
+        "pod, expected_disk_size_gb",
+        [
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(
+                                    requests={"ephemeral-storage": "15G"}, limits={"ephemeral-storage": "20G"}
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                "50",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(
+                                    requests={"ephemeral-storage": "17G"}, limits={"ephemeral-storage": "15G"}
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                "47",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(
+                                    requests={"wrong-parameter": "17G"}, limits={"wrong-parameter": "15G"}
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                "30",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(
+                                    requests={"ephemeral-storage": "17Gi"},
+                                    limits={"ephemeral-storage": "15Gi"},
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                "49",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(
+                                    requests={"ephemeral-storage": "15Gi"},
+                                    limits={"ephemeral-storage": "17Gi"},
+                                ),
+                            )
+                        ]
+                    )
+                ),
+                "49",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(limits={"ephemeral-storage": "11G"}),
+                            )
+                        ]
+                    )
+                ),
+                "41",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(requests={"ephemeral-storage": "12G"}),
+                            )
+                        ]
+                    )
+                ),
+                "42",
+            ),
+            (
+                k8s.V1Pod(spec=k8s.V1PodSpec(containers=[k8s.V1Container(name="base")])),
+                "30",
+            ),
+            (
+                k8s.V1Pod(
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                resources=k8s.V1ResourceRequirements(requests={"ephemeral-storage": "200G"}),
+                            )
+                        ]
+                    )
+                ),
+                "130",
+            ),
+        ],
+    )
+    @mock.patch.dict("os.environ", {"GCP_TENANT_PROJECT": "test-project-234"})
+    def test_get_peer_vm_pod_metadata_disk_size_gb(self, pod, expected_disk_size_gb):
+        actual_pod_metadata = _get_peer_vm_pod_metadata(pod)
+
+        actual_disk_size_gb = yaml.safe_load(actual_pod_metadata.annotations.get("node.gke.io/gce-vm"))[
+            "selector"
+        ]["matchLabels"]["diskSizeGb"]
+        assert actual_disk_size_gb == expected_disk_size_gb
+
+    @pytest.mark.parametrize(
+        "resources, expected_machine_type",
+        [
+            (None, "e2-custom-small-2048"),
+        ]
+        + [
+            # Tests for CPU.
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "1"}, limits={"cpu": "2"}),
+                "e2-custom-2-8192",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "1"}, limits={"cpu": "2"}),
+                "e2-custom-2-8192",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={}, limits={}),
+                "e2-custom-small-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "3000m"}, limits={"cpu": "1000m"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "2.5"}, limits={"cpu": "0.5"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "1000m"}, limits={"cpu": "3000m"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "0.5"}, limits={"cpu": "2.5"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"cpu": "6.5"}),
+                "e2-custom-8-32768",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "3.89"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "0.1"}),
+                "e2-custom-micro-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "0.26"}),
+                "e2-custom-small-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "0.6"}),
+                "e2-custom-medium-4096",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "3.9"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "4.0000000001"}),
+                "e2-custom-4-16384",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "300"}),
+                "e2-custom-32-131072",
+            ),
+        ]
+        + [
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": str(x)}),
+                f"e2-custom-{x}-{x * 4 * 1024}",
+            )
+            for x in range(2, 34, 2)
+        ]
+        # Tests for memory.
+        + [
+            (
+                k8s.V1ResourceRequirements(requests={"wrong-key": "1G"}, limits={"wrong-key": "2G"}),
+                "e2-custom-small-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"memory": "1G"}, limits={"memory": "3G"}),
+                "e2-custom-small-3072",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"memory": "3G"}, limits={"memory": "1G"}),
+                "e2-custom-small-3072",
+            ),
+            (
+                k8s.V1ResourceRequirements(requests={"memory": "1G"}),
+                "e2-custom-small-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"memory": "4G"}),
+                "e2-custom-small-4096",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"memory": "2.5G"}),
+                "e2-custom-small-3072",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"memory": "3.0000000001G"}),
+                "e2-custom-small-3072",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"memory": "0.1G"}),
+                "e2-custom-small-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "32", "memory": "128G"}),
+                "e2-custom-32-131072",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "32", "memory": "129G"}),
+                "e2-custom-32-131072",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "4", "memory": "0.1G"}),
+                "e2-custom-4-2048",
+            ),
+            (
+                k8s.V1ResourceRequirements(limits={"cpu": "4", "memory": "100G"}),
+                "e2-custom-4-32768",
+            ),
+        ],
+    )
+    def test_get_peer_vm_machine_type(self, resources, expected_machine_type):
+        actual_machine_type = _get_peer_vm_machine_type(resources)
+
+        assert actual_machine_type == expected_machine_type
