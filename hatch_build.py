8a3e25be521ac4e17c746d05137a067e39c52de2
Cloud Composer Team <no-reply@google.com>
2025-07-24 09:43:23 +0000

    fixup! Composer dependencies patch
    - Pin cncf providers while we solve Internal bug
    - Add apache-airflow-providers-apache-kafka.
    
    Change-Id: I50ff0519ba90e742d3b83a9fe3fab789c474e70c
    GitOrigin-RevId: 01f6ee0fd0f03748e1c51ea278d66ba56e3ccfd8

--- hatch_build.py
+++ hatch_build.py
@@ -504,13 +504,15 @@ COMPOSER_DEPENDENCIES = [
     # Beam provider 6.1.0 (next version) is not compatible with google provider 15.1.0. We should remove
     # constraint from beam provider once google provider package is upgraded to 16.0.0+.
     "apache-airflow-providers-apache-beam<=6.0.4",
+    "apache-airflow-providers-apache-kafka",
     # Celery provider 3.9.0 breaks for airflow < 3, will be fixed in 3.9.1
     # TODO: Internal bug - Also Celery provider >3.10.0 breaks for Airflow <= 2.10.5
     "apache-airflow-providers-celery <=3.10.0,!=3.9.0",
     # In version 10.4.3 of the kubernetes provider community introduced AF 2.x.x incompatible import.
     # https://github.com/apache/airflow/pull/49186 But revert the changes in 10.5.0
     # https://github.com/apache/airflow/pull/50651
-    "apache-airflow-providers-cncf-kubernetes!=10.4.3",
+    # TODO: Internal bug: Remove the "<10.6.0" constraint once the issue is resolved in Composer 3.
+    "apache-airflow-providers-cncf-kubernetes!=10.4.3,<10.6.0",
     "apache-airflow-providers-dbt-cloud",
     "apache-airflow-providers-google",
     "apache-airflow-providers-hashicorp",
