f66f04f2ea072a0d3ec4cd1c4ba2251e6bedba97
Cloud Composer Team <no-reply@google.com>
2025-09-05 09:37:10 +0200

    fixup! Composer RBAC patch
    Add ALL_RBAC_ROLES_EXTRA_PERMISSIONS to all RBAC roles.
    (backporting from https://cloud-airflow-review.git.corp.google.com/c/composer-airflow/+/43053)
    
    Change-Id: I98ed10ec0b4cf7ded14539c6368d7bd541080b4a
    GitOrigin-RevId: 43effbc7b1b986ced2dc7f38961f1afda9f4ec95

--- airflow-core/tests/unit/composer/patches/rbac/test_composer_airflow_security_manager.py
+++ airflow-core/tests/unit/composer/patches/rbac/test_composer_airflow_security_manager.py
@@ -15,11 +15,17 @@
 
 from __future__ import annotations
 
+import random
+import string
+
 from flask_appbuilder.const import AUTH_REMOTE_USER
 
+from airflow.composer.patches.core.configuration import ALL_RBAC_ROLES_EXTRA_PERMISSIONS
 from airflow.composer.patches.rbac.composer_airflow_security_manager import ComposerAirflowSecurityManager
 from airflow.composer.patches.rbac.composer_auth_remote_user_view import ComposerAuthRemoteUserView
 from airflow.providers.fab.www.app import create_app
+
+from tests_common.test_utils.config import conf_vars
 
 
 class TestComposerAirflowSecurityManager:
@@ -29,3 +35,32 @@ class TestComposerAirflowSecurityManager:
 
         assert ComposerAirflowSecurityManager.authremoteuserview == ComposerAuthRemoteUserView
         assert app.config["AUTH_TYPE"] == AUTH_REMOTE_USER
+
+    @conf_vars(
+        {("core", "auth_manager"): "airflow.composer.patches.rbac.composer_auth_manager.ComposerAuthManager"}
+    )
+    def test_sync_roles(self):
+        resource_name = "".join(random.choice(string.ascii_uppercase) for _ in range(6))
+        ALL_RBAC_ROLES_EXTRA_PERMISSIONS.append(("menu_access", resource_name))
+        custom_role = "".join(random.choice(string.ascii_uppercase) for _ in range(6))
+
+        def _has_permission(sm, role_name):
+            role = sm.find_role(role_name)
+            for permission in role.permissions:
+                if permission.action.name == "menu_access" and permission.resource.name == resource_name:
+                    return True
+
+            return False
+
+        app = create_app(enable_plugins=False)
+        app.appbuilder.sm.add_role(custom_role)
+
+        # Built-in roles should get permission already, since sync_roles() is called during app creation.
+        assert _has_permission(app.appbuilder.sm, "Admin")
+        # Custom role shouldn't have permission yet, because the role was added after app created.
+        assert not _has_permission(app.appbuilder.sm, custom_role)
+
+        app.appbuilder.sm.sync_roles()
+
+        assert _has_permission(app.appbuilder.sm, "Admin")
+        assert _has_permission(app.appbuilder.sm, custom_role)
