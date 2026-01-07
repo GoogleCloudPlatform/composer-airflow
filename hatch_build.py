149a311ebb20cae1e032a5f104b96cffffe73b51
Cloud Composer Team <no-reply@google.com>
2026-01-07 09:51:51 +0000

    fixup! Composer dependencies patch
    Change-Id: Ie9160e4c82265dc0cf2b4ad7eb4272cc787d6d55
    GitOrigin-RevId: 10c6650dfe765878a33c9a1840fd2147b83aec2a

--- hatch_build.py
+++ hatch_build.py
@@ -527,7 +527,7 @@ COMPOSER_DEPENDENCIES = [
     "confluent-kafka",
     "crcmod<2.0",
     "cryptography",
-    "dbt-bigquery",
+    "dbt-bigquery<=1.10.3",  # Composer dependency, constraint to help pip resolver
     "dbt-core",
     "firebase-admin",
     "flask_limiter<=3.12",  # Only as a constraint
@@ -568,6 +568,7 @@ COMPOSER_DEPENDENCIES = [
     "tensorflow",
     # TODO: Internal bug - Review if possible to remove constraint in Airflow >= 2.11
     "universal-pathlib<=0.2.6",  # Only as constraint
+    "urllib3>=2.6.0",  # Only as constraint to solve SV
     "virtualenv>=20.24.0",
     "websockets",
     # Versions < 2.2.3 contain security vulnerabilities.
