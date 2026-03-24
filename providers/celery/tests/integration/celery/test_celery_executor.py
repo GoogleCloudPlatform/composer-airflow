e0e1ac9bdff067a6d9babe300536618db7c63f7a
Aaron Chen <nailo2c@gmail.com>
2026-03-23 23:23:31 -0700

    Update celery provider conf imports to use common compat SDK (#64134)
    * Update celery provider conf imports to use common compat SDK
    
    * Keep cli files using airflow.configuration.conf
    
    GitOrigin-RevId: c09f02de555049e36ba45a3a554d3e59dfa4ca52

--- providers/celery/tests/integration/celery/test_celery_executor.py
+++ providers/celery/tests/integration/celery/test_celery_executor.py
@@ -40,12 +40,11 @@ from kubernetes.client import models as k8s
 from uuid6 import uuid7
 
 from airflow._shared.timezones import timezone
-from airflow.configuration import conf
 from airflow.executors import workloads
 from airflow.executors.workloads.task import TaskInstanceDTO
 from airflow.models.dag import DAG
 from airflow.models.taskinstance import TaskInstance
-from airflow.providers.common.compat.sdk import AirflowException, AirflowTaskTimeout, TaskInstanceKey
+from airflow.providers.common.compat.sdk import AirflowException, AirflowTaskTimeout, TaskInstanceKey, conf
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.sdk import BaseOperator
 from airflow.utils.state import State
