adfe7d55ff5832fb0b8a54148e38ea1cc1a07dec
Cloud Composer Team <no-reply@google.com>
2026-03-30 15:21:39 +0000

    fixup! Composer dependencies patch
    Add constraint for google-api-core due to performance issue
    
    Change-Id: I8e7f32f87ee35b50fe99593abde40da44d1a50fb
    GitOrigin-RevId: 374c96d9508de63dc8f8a774b943da0463f7d670

--- hatch_build.py
+++ hatch_build.py
@@ -526,6 +526,8 @@ COMPOSER_DEPENDENCIES = [
     # Due to security vulnerability Flower version >= 2.0.0 required.
     "flower>=2.0.0",
     "gcsfs",
+    # TODO: Due to performance issue see: Internal bug, remove once bug is fixed
+    "google-api-core<=2.27.0",
     "google-apitools",
     "google-cloud-aiplatform[evaluation]",
     "google-cloud-asset",
