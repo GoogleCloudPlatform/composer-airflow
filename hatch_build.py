4c4541f2249846dba818b879e5358a119f52d478
Cloud Composer Team <no-reply@google.com>
2026-07-02 14:29:28 +0000

    fixup! Composer dependencies patch
    Remove constraint on pytest, as issue is resolved
    
    Change-Id: I9d7906f207085259ab373b744bb92c9a07ddc533
    (cherry picked from commit ebfa73e90bcb8256d3f56f5787bf2149b3547bf7)
    GitOrigin-RevId: 037929e67e92f301592d6c551773146da690e8b4

--- hatch_build.py
+++ hatch_build.py
@@ -560,10 +560,6 @@ COMPOSER_DEPENDENCIES = [
     "pyOpenSSL",
     "pipdeptree",
     "pygments>2.15.0",
-    # Only as a constraint, we can remove once it's fixed as it's not a Composer dependency
-    # https://github.com/pytest-dev/pytest/issues/13477
-    # https://github.com/apache/airflow/pull/58166
-    "pytest!=8.4.0,<9.0.0",
     "setuptools>=78.1.1",  # Only as a constraint to avoid vulnerabilities
     # Only as a constraint, we can remove once it's fixed as it's not a Composer dependency
     # https://github.com/googleapis/python-spanner-sqlalchemy/issues/682
